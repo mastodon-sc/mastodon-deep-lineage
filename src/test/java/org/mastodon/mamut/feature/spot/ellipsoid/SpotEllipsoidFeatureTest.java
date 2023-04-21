4abaa69e02dd77d2ab780afe697d4e9b94a0852b
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-04-21 13:51:37 +0200

    Improve SpotEllipsoidFeatureTest
    * Add test for correct serialization/deserialization
    * Add test for correct invalidation

--- src/test/java/org/mastodon/mamut/feature/spot/ellipsoid/SpotEllipsoidFeatureTest.java
+++ src/test/java/org/mastodon/mamut/feature/spot/ellipsoid/SpotEllipsoidFeatureTest.java
@@ -1,16 +1,21 @@
 package org.mastodon.mamut.feature.spot.ellipsoid;
 
 import org.junit.Test;
-import org.mastodon.feature.FeatureProjection;
+import org.mastodon.feature.Feature;
+import org.mastodon.feature.FeatureProjectionKey;
+import org.mastodon.mamut.feature.FeatureSerializerTestUtils;
 import org.mastodon.mamut.feature.branch.FeatureComputerTestUtils;
 import org.mastodon.mamut.feature.exampleGraph.ExampleGraph3;
+import org.mastodon.mamut.model.Model;
 import org.mastodon.mamut.model.Spot;
 import org.scijava.Context;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SpotEllipsoidFeatureTest
 {
+
 	@Test
 	public void testEllipsoidFeature()
 	{
@@ -22,31 +27,76 @@ public class SpotEllipsoidFeatureTest
 			double[][] covarianceMatrix = new double[][] { { 6, 2, 3 }, { 2, 7, 4 }, { 3, 4, 8 } };
 			exampleGraph3.spot0.setCovariance( covarianceMatrix );
 
-			// compute features
-			FeatureProjection< Spot > shortAxis = FeatureComputerTestUtils.getSpotFeatureProjection( context, exampleGraph3,
-					SpotEllipsoidFeature.SPOT_ELLIPSOID_FEATURE_SPEC, SpotEllipsoidFeature.SHORT_SEMI_AXIS_PROJECTION_SPEC );
-			FeatureProjection< Spot > middleAxis = FeatureComputerTestUtils.getSpotFeatureProjection( context, exampleGraph3,
-					SpotEllipsoidFeature.SPOT_ELLIPSOID_FEATURE_SPEC, SpotEllipsoidFeature.MIDDLE_SEMI_AXIS_PROJECTION_SPEC );
-			FeatureProjection< Spot > longAxis = FeatureComputerTestUtils.getSpotFeatureProjection( context, exampleGraph3,
-					SpotEllipsoidFeature.SPOT_ELLIPSOID_FEATURE_SPEC, SpotEllipsoidFeature.LONG_SEMI_AXIS_PROJECTION_SPEC );
-			FeatureProjection< Spot > volume = FeatureComputerTestUtils.getSpotFeatureProjection( context, exampleGraph3,
-					SpotEllipsoidFeature.SPOT_ELLIPSOID_FEATURE_SPEC, SpotEllipsoidFeature.VOLUME_PROJECTION_SPEC );
+			// compute feature
+			Feature< Spot > ellipsoidFeature =
+					FeatureComputerTestUtils.getSpotFeature( context, exampleGraph3, SpotEllipsoidFeature.SPOT_ELLIPSOID_FEATURE_SPEC );
 
 			// eigenvalues for given covariance matrix
 			// cf. https://matrixcalc.org/de/vectors.html#eigenvectors({{6, 2, 3}, {2, 7, 4}, {3, 4, 8}})
 			final double[] eigenValues = new double[] { 3.270d, 4.442d, 13.288d };
 
 			// compute semi-axes from eigenvalues
-			double shortSemiAxis = Math.sqrt( eigenValues[ 0 ] );
-			double middleSemiAxis = Math.sqrt( eigenValues[ 1 ] );
-			double longSemiAxis = Math.sqrt( eigenValues[ 2 ] );
+			double expectedShortAxis = Math.sqrt( eigenValues[ 0 ] );
+			double expectedMiddleAxis = Math.sqrt( eigenValues[ 1 ] );
+			double expectedLongAxis = Math.sqrt( eigenValues[ 2 ] );
+			double expectedVolume = expectedShortAxis * expectedMiddleAxis * expectedLongAxis * 4d / 3d * Math.PI;
 
-			assertEquals( shortSemiAxis, shortAxis.value( exampleGraph3.spot0 ), 0.001d );
-			assertEquals( middleSemiAxis, middleAxis.value( exampleGraph3.spot0 ), 0.001d );
-			assertEquals( longSemiAxis, longAxis.value( exampleGraph3.spot0 ), 0.001d );
+			// check that the features are computed correctly
+			assertEquals( expectedShortAxis,
+					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.SHORT_SEMI_AXIS_PROJECTION_SPEC ) )
+							.value( exampleGraph3.spot0 ),
+					0.001d );
+			assertEquals( expectedMiddleAxis,
+					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.MIDDLE_SEMI_AXIS_PROJECTION_SPEC ) )
+							.value( exampleGraph3.spot0 ),
+					0.001d );
+			assertEquals( expectedLongAxis,
+					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.LONG_SEMI_AXIS_PROJECTION_SPEC ) )
+							.value( exampleGraph3.spot0 ),
+					0.001d );
 			// volume of ellipsoid https://en.wikipedia.org/wiki/Ellipsoid#Volume
-			assertEquals( shortSemiAxis * middleSemiAxis * longSemiAxis * 4d / 3d * Math.PI,
-					volume.value( exampleGraph3.spot0 ), 0.01d );
+			assertEquals( expectedVolume, ellipsoidFeature
+					.project( FeatureProjectionKey.key( SpotEllipsoidFeature.VOLUME_PROJECTION_SPEC ) ).value( exampleGraph3.spot0 ),
+					0.01d );
+
+			Model model = exampleGraph3.getModel();
+			FeatureSerializerTestUtils.saveAndReload( context, exampleGraph3.getModel(), ellipsoidFeature );
+
+			// check that the feature has correct values after saving and reloading
+			SpotEllipsoidFeature ellipsoidFeatureReloaded = ( SpotEllipsoidFeature ) model.getFeatureModel()
+					.getFeature( SpotEllipsoidFeature.SPOT_ELLIPSOID_FEATURE_SPEC );
+			assertEquals( expectedShortAxis,
+					ellipsoidFeatureReloaded.project( FeatureProjectionKey.key( SpotEllipsoidFeature.SHORT_SEMI_AXIS_PROJECTION_SPEC ) )
+							.value( exampleGraph3.spot0 ),
+					0.001d );
+			assertEquals( expectedMiddleAxis, ellipsoidFeatureReloaded
+					.project( FeatureProjectionKey.key( SpotEllipsoidFeature.MIDDLE_SEMI_AXIS_PROJECTION_SPEC ) )
+					.value( exampleGraph3.spot0 ),
+					0.001d );
+			assertEquals( expectedLongAxis,
+					ellipsoidFeatureReloaded.project( FeatureProjectionKey.key( SpotEllipsoidFeature.LONG_SEMI_AXIS_PROJECTION_SPEC ) )
+							.value( exampleGraph3.spot0 ),
+					0.001d );
+			assertEquals( expectedVolume,
+					ellipsoidFeatureReloaded.project( FeatureProjectionKey.key( SpotEllipsoidFeature.VOLUME_PROJECTION_SPEC ) )
+							.value( exampleGraph3.spot0 ),
+					0.01d );
+
+			// test, if features are NaN after invalidation
+			ellipsoidFeature.invalidate( exampleGraph3.spot0 );
+			assertTrue( Double.isNaN(
+					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.SHORT_SEMI_AXIS_PROJECTION_SPEC ) )
+							.value( exampleGraph3.spot0 ) ) );
+			assertTrue( Double.isNaN(
+					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.MIDDLE_SEMI_AXIS_PROJECTION_SPEC ) )
+							.value( exampleGraph3.spot0 ) ) );
+			assertTrue( Double.isNaN(
+					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.LONG_SEMI_AXIS_PROJECTION_SPEC ) )
+							.value( exampleGraph3.spot0 ) ) );
+			assertTrue( Double.isNaN(
+					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.VOLUME_PROJECTION_SPEC ) )
+							.value( exampleGraph3.spot0 ) ) );
+
 		}
 	}
 }
