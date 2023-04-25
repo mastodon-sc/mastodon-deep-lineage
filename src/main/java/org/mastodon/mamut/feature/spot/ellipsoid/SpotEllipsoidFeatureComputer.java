9be553a869ed704be411daf3d1cfe2c74dba5c4a
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-04-25 10:05:42 +0200

    Update copyright info in SpotEllipsoidFeature classes

--- src/main/java/org/mastodon/mamut/feature/spot/ellipsoid/SpotEllipsoidFeatureComputer.java
+++ src/main/java/org/mastodon/mamut/feature/spot/ellipsoid/SpotEllipsoidFeatureComputer.java
@@ -2,7 +2,7 @@
  * #%L
  * Mastodon
  * %%
- * Copyright (C) 2014 - 2022 Tobias Pietzsch, Jean-Yves Tinevez
+ * Copyright (C) 2022 - 2023 Stefan Hahmann
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
