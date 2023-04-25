d06d151604a1df218d3c52d13b23b7177ca3bf5f
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-04-25 17:13:19 +0200

    Remove unrequired class ExampleGraph3 and instead initialize Graph directly in SpotEllipsoidFeatureTest

--- src/test/java/org/mastodon/mamut/feature/spot/ellipsoid/SpotEllipsoidFeatureTest.java
+++ src/test/java/org/mastodon/mamut/feature/spot/ellipsoid/SpotEllipsoidFeatureTest.java
@@ -5,8 +5,8 @@ import org.mastodon.feature.Feature;
 import org.mastodon.feature.FeatureProjectionKey;
 import org.mastodon.mamut.feature.FeatureSerializerTestUtils;
 import org.mastodon.mamut.feature.branch.FeatureComputerTestUtils;
-import org.mastodon.mamut.feature.exampleGraph.ExampleGraph3;
 import org.mastodon.mamut.model.Model;
+import org.mastodon.mamut.model.ModelGraph;
 import org.mastodon.mamut.model.Spot;
 import org.scijava.Context;
 
@@ -15,21 +15,28 @@ import static org.junit.Assert.assertTrue;
 
 public class SpotEllipsoidFeatureTest
 {
+	private final Model model = new Model();
+
+	private final ModelGraph graph = model.getGraph();
+
+	private final Spot spot = initEllipsoidSpot( graph );
+
+	private static Spot initEllipsoidSpot( ModelGraph graph )
+	{
+		Spot spot = graph.addVertex();
+		spot.init( 1, new double[] { 1, 2, 3 }, 0 );
+		spot.setCovariance( new double[][] { { 6, 2, 3 }, { 2, 7, 4 }, { 3, 4, 8 } } );
+		return spot;
+	}
 
 	@Test
 	public void testEllipsoidFeature()
 	{
 		try (Context context = new Context())
 		{
-			ExampleGraph3 exampleGraph3 = new ExampleGraph3();
-
-			// set an example covariance matrix
-			double[][] covarianceMatrix = new double[][] { { 6, 2, 3 }, { 2, 7, 4 }, { 3, 4, 8 } };
-			exampleGraph3.spot0.setCovariance( covarianceMatrix );
-
 			// compute feature
 			Feature< Spot > ellipsoidFeature =
-					FeatureComputerTestUtils.getSpotFeature( context, exampleGraph3, SpotEllipsoidFeature.SPOT_ELLIPSOID_FEATURE_SPEC );
+					FeatureComputerTestUtils.getSpotFeature( context, model, SpotEllipsoidFeature.SPOT_ELLIPSOID_FEATURE_SPEC );
 
 			// eigenvalues for given covariance matrix
 			// cf. https://matrixcalc.org/de/vectors.html#eigenvectors({{6, 2, 3}, {2, 7, 4}, {3, 4, 8}})
@@ -44,58 +51,57 @@ public class SpotEllipsoidFeatureTest
 			// check that the features are computed correctly
 			assertEquals( expectedShortAxis,
 					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.SHORT_SEMI_AXIS_PROJECTION_SPEC ) )
-							.value( exampleGraph3.spot0 ),
+							.value( spot ),
 					0.001d );
 			assertEquals( expectedMiddleAxis,
 					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.MIDDLE_SEMI_AXIS_PROJECTION_SPEC ) )
-							.value( exampleGraph3.spot0 ),
+							.value( spot ),
 					0.001d );
 			assertEquals( expectedLongAxis,
 					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.LONG_SEMI_AXIS_PROJECTION_SPEC ) )
-							.value( exampleGraph3.spot0 ),
+							.value( spot ),
 					0.001d );
 			// volume of ellipsoid https://en.wikipedia.org/wiki/Ellipsoid#Volume
 			assertEquals( expectedVolume, ellipsoidFeature
-					.project( FeatureProjectionKey.key( SpotEllipsoidFeature.VOLUME_PROJECTION_SPEC ) ).value( exampleGraph3.spot0 ),
+					.project( FeatureProjectionKey.key( SpotEllipsoidFeature.VOLUME_PROJECTION_SPEC ) ).value( spot ),
 					0.01d );
 
-			Model model = exampleGraph3.getModel();
-			FeatureSerializerTestUtils.saveAndReload( context, exampleGraph3.getModel(), ellipsoidFeature );
+			FeatureSerializerTestUtils.saveAndReload( context, model, ellipsoidFeature );
 
 			// check that the feature has correct values after saving and reloading
 			SpotEllipsoidFeature ellipsoidFeatureReloaded = ( SpotEllipsoidFeature ) model.getFeatureModel()
 					.getFeature( SpotEllipsoidFeature.SPOT_ELLIPSOID_FEATURE_SPEC );
 			assertEquals( expectedShortAxis,
 					ellipsoidFeatureReloaded.project( FeatureProjectionKey.key( SpotEllipsoidFeature.SHORT_SEMI_AXIS_PROJECTION_SPEC ) )
-							.value( exampleGraph3.spot0 ),
+							.value( spot ),
 					0.001d );
 			assertEquals( expectedMiddleAxis, ellipsoidFeatureReloaded
 					.project( FeatureProjectionKey.key( SpotEllipsoidFeature.MIDDLE_SEMI_AXIS_PROJECTION_SPEC ) )
-					.value( exampleGraph3.spot0 ),
+					.value( spot ),
 					0.001d );
 			assertEquals( expectedLongAxis,
 					ellipsoidFeatureReloaded.project( FeatureProjectionKey.key( SpotEllipsoidFeature.LONG_SEMI_AXIS_PROJECTION_SPEC ) )
-							.value( exampleGraph3.spot0 ),
+							.value( spot ),
 					0.001d );
 			assertEquals( expectedVolume,
 					ellipsoidFeatureReloaded.project( FeatureProjectionKey.key( SpotEllipsoidFeature.VOLUME_PROJECTION_SPEC ) )
-							.value( exampleGraph3.spot0 ),
+							.value( spot ),
 					0.01d );
 
 			// test, if features are NaN after invalidation
-			ellipsoidFeature.invalidate( exampleGraph3.spot0 );
+			ellipsoidFeature.invalidate( spot );
 			assertTrue( Double.isNaN(
 					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.SHORT_SEMI_AXIS_PROJECTION_SPEC ) )
-							.value( exampleGraph3.spot0 ) ) );
+							.value( spot ) ) );
 			assertTrue( Double.isNaN(
 					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.MIDDLE_SEMI_AXIS_PROJECTION_SPEC ) )
-							.value( exampleGraph3.spot0 ) ) );
+							.value( spot ) ) );
 			assertTrue( Double.isNaN(
 					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.LONG_SEMI_AXIS_PROJECTION_SPEC ) )
-							.value( exampleGraph3.spot0 ) ) );
+							.value( spot ) ) );
 			assertTrue( Double.isNaN(
 					ellipsoidFeature.project( FeatureProjectionKey.key( SpotEllipsoidFeature.VOLUME_PROJECTION_SPEC ) )
-							.value( exampleGraph3.spot0 ) ) );
+							.value( spot ) ) );
 
 		}
 	}
