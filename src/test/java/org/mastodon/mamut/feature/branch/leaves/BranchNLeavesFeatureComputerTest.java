12a3523445e6c84843b23d9f635f433183494ac1
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-05-08 10:38:24 +0200

    Replace ExampleGraph with Model in Branch Feature Tests

--- src/test/java/org/mastodon/mamut/feature/branch/leaves/BranchNLeavesFeatureComputerTest.java
+++ src/test/java/org/mastodon/mamut/feature/branch/leaves/BranchNLeavesFeatureComputerTest.java
@@ -19,7 +19,7 @@ public class BranchNLeavesFeatureComputerTest
 		{
 			ExampleGraph1 exampleGraph1 = new ExampleGraph1();
 			FeatureProjection< BranchSpot > featureProjection =
-					FeatureComputerTestUtils.getFeatureProjection( context, exampleGraph1,
+					FeatureComputerTestUtils.getFeatureProjection( context, exampleGraph1.getModel(),
 							BranchNLeavesFeature.BRANCH_N_LEAVES_FEATURE_SPEC, BranchNLeavesFeature.PROJECTION_SPEC );
 
 			assertEquals( 1, featureProjection.value( exampleGraph1.branchSpotA ), 0 );
@@ -33,7 +33,7 @@ public class BranchNLeavesFeatureComputerTest
 		{
 			ExampleGraph2 exampleGraph2 = new ExampleGraph2();
 			FeatureProjection< BranchSpot > featureProjection =
-					FeatureComputerTestUtils.getFeatureProjection( context, exampleGraph2,
+					FeatureComputerTestUtils.getFeatureProjection( context, exampleGraph2.getModel(),
 							BranchNLeavesFeature.BRANCH_N_LEAVES_FEATURE_SPEC, BranchNLeavesFeature.PROJECTION_SPEC );
 
 			assertEquals( 3, featureProjection.value( exampleGraph2.branchSpotA ), 0 );
