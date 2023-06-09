28c3508bf9dc21572a48638e2ac38f28db648ef4
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-06-09 09:45:30 +0200

    Added more test case to unit test and refactored unit test

--- src/test/java/org/mastodon/mamut/util/ZhangUnorderedTreeEditDistanceTest.java
+++ src/test/java/org/mastodon/mamut/util/ZhangUnorderedTreeEditDistanceTest.java
@@ -8,8 +8,11 @@ import static org.junit.Assert.assertEquals;
 
 public class ZhangUnorderedTreeEditDistanceTest
 {
+
+	private static final String ATTRIBUTE = "node_weight";
+
 	@Test
-	public void testzhang_edit_distance()
+	public void testZhangEditDistance()
 	{
 		BiFunction< Object, Object, Integer > costFunction = ( o1, o2 ) -> {
 			if ( o2 == null )
@@ -18,34 +21,82 @@ public class ZhangUnorderedTreeEditDistanceTest
 				return Math.abs( ( Integer ) o1 - ( Integer ) o2 );
 		};
 
-		Tree tree2 = getExampleTree2();
-		Tree tree5 = getExampleTree5();
+		Tree emptyTree = emptyTree();
+		Tree tree1 = tree1();
+		Tree tree2 = tree2();
+		Tree tree3 = tree3();
+		Tree tree4 = tree4();
+		Tree tree5 = tree5();
+		Tree tree6 = tree6();
+		Tree tree7 = tree7();
 
-		int distance = ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree2, tree5, "node_weight", costFunction, false );
-		assertEquals( 20, distance );
+		// similarities with empty trees
+
+		// 60, because: 3 nodes with a total weight of 60 are added to the empty tree
+		assertEquals( 60, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree1, emptyTree, ATTRIBUTE, costFunction, false ) );
+		assertEquals( 60, ZhangUnorderedTreeEditDistance.zhang_edit_distance( emptyTree, tree1, ATTRIBUTE, costFunction, false ) );
+		// 60, because: 3 nodes with a total weight of 60 are added to the empty tree
+		assertEquals( 60, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree2, emptyTree, ATTRIBUTE, costFunction, false ) );
+		assertEquals( 60, ZhangUnorderedTreeEditDistance.zhang_edit_distance( emptyTree, tree2, ATTRIBUTE, costFunction, false ) );
+		// 104, because: 3 nodes with a total weight of 104 are added to the empty tree
+		assertEquals( 104, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree3, emptyTree, ATTRIBUTE, costFunction, false ) );
+		assertEquals( 104, ZhangUnorderedTreeEditDistance.zhang_edit_distance( emptyTree, tree3, ATTRIBUTE, costFunction, false ) );
+		// 104, because: 3 nodes with a total weight of 104 are added to the empty tree
+		assertEquals( 104, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree4, emptyTree, ATTRIBUTE, costFunction, false ) );
+		assertEquals( 104, ZhangUnorderedTreeEditDistance.zhang_edit_distance( emptyTree, tree4, ATTRIBUTE, costFunction, false ) );
+
+		// similarity of trees against other trees
+
+		// 20, because: 2 nodes with a difference of 10 each need to be changed
+		assertEquals( 20, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree1, tree2, ATTRIBUTE, costFunction, false ) );
+		assertEquals( 20, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree2, tree1, ATTRIBUTE, costFunction, false ) );
+		// 4, because:
+		// 2 nodes are removed (weight 1 each), e.g. node 2 and node 3 in tree 3
+		// 2 nodes are added (weight 1 each), e.g. node 4 and node 5 in tree 4
+		assertEquals( 4, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree3, tree4, ATTRIBUTE, costFunction, false ) );
+		assertEquals( 4, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree4, tree3, ATTRIBUTE, costFunction, false ) );
+
+		// 49, because: one node has a difference of 1
+		// two nodes have a difference of 24 each
+		assertEquals( 49, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree5, tree6, ATTRIBUTE, costFunction, false ) );
+		assertEquals( 49, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree6, tree5, ATTRIBUTE, costFunction, false ) );
+
+		// 69, because:
+		// one node has a difference of 1
+		// two nodes have a difference of 24 each
+		// two extra nodes are added with a weight of 10 each
+		assertEquals( 69, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree5, tree7, ATTRIBUTE, costFunction, false ) );
+		assertEquals( 69, ZhangUnorderedTreeEditDistance.zhang_edit_distance( tree7, tree5, ATTRIBUTE, costFunction, false ) );
 	}
 
-	private Tree getExampleTree2()
+	private Tree emptyTree()
+	{
+		Tree node1 = new Tree();
+		node1.addAttributeToId( ATTRIBUTE, 0 );
+		return node1;
+	}
+
+	private Tree tree1()
 	{
 		//   					   node1(node_weight=20)
 		//                ┌-─────────┴─────────────┐
 		//                │                        │
 		//              node2(node_weight=10)    node3(node_weight=30)
 		Tree node1 = new Tree();
-		node1.getMyAttributes().put( "node_weight", 20 );
+		node1.addAttributeToId( ATTRIBUTE, 20 );
 
 		Tree node2 = new Tree();
-		node2.addAttributeToId( "node_weight", 10 );
+		node2.addAttributeToId( ATTRIBUTE, 10 );
 		node1.addSubtree( node2 );
 
 		Tree node3 = new Tree();
-		node3.addAttributeToId( "node_weight", 30 );
+		node3.addAttributeToId( ATTRIBUTE, 30 );
 		node1.addSubtree( node3 );
 
 		return node1;
 	}
 
-	private Tree getExampleTree5()
+	private Tree tree2()
 	{
 		//  				       node1(node_weight=30)
 		//                ┌-─────────┴─────────────┐
@@ -53,15 +104,155 @@ public class ZhangUnorderedTreeEditDistanceTest
 		//              node2(node_weight=10)    node3(node_weight=20)
 
 		Tree node1 = new Tree();
-		node1.addAttributeToId( "node_weight", 30 );
+		node1.addAttributeToId( ATTRIBUTE, 30 );
 
 		Tree node2 = new Tree();
-		node2.addAttributeToId( "node_weight", 10 );
+		node2.addAttributeToId( ATTRIBUTE, 10 );
 		node1.addSubtree( node2 );
 
 		Tree node3 = new Tree();
-		node3.addAttributeToId( "node_weight", 20 );
+		node3.addAttributeToId( ATTRIBUTE, 20 );
 		node1.addSubtree( node3 );
+
+		return node1;
+	}
+
+	private Tree tree3()
+	{
+		//
+		//                        node1(node_weight=1)
+		//               ┌-─────────┴─────────────┐
+		//               │                        │
+		//             node2(node_weight=1)     node3(node_weight=1)
+		//    ┌-─────────┴─────────────┐
+		//    │                        │
+		//  node4(node_weight=1)     node5(node_weight=100)
+
+		Tree node1 = new Tree();
+		node1.addAttributeToId( ATTRIBUTE, 1 );
+
+		Tree node2 = new Tree();
+		node2.addAttributeToId( ATTRIBUTE, 1 );
+		node1.addSubtree( node2 );
+
+		Tree node3 = new Tree();
+		node3.addAttributeToId( ATTRIBUTE, 1 );
+		node1.addSubtree( node3 );
+
+		Tree node4 = new Tree();
+		node4.addAttributeToId( ATTRIBUTE, 1 );
+		node2.addSubtree( node4 );
+
+		Tree node5 = new Tree();
+		node5.addAttributeToId( ATTRIBUTE, 100 );
+		node2.addSubtree( node5 );
+
+		return node1;
+	}
+
+	private Tree tree4()
+	{
+		//                       node1(node_weight=1)
+		//              ┌-─────────┴─────────────┐
+		//              │                        │
+		//            node2(node_weight=100)   node3(node_weight=1)
+		//   ┌-─────────┴─────────────┐
+		//   │                        │
+		// node4(node_weight=1)     node5(node_weight=1)
+
+		Tree node1 = new Tree();
+		node1.addAttributeToId( ATTRIBUTE, 1 );
+
+		Tree node2 = new Tree();
+		node2.addAttributeToId( ATTRIBUTE, 100 );
+		node1.addSubtree( node2 );
+
+		Tree node3 = new Tree();
+		node3.addAttributeToId( ATTRIBUTE, 1 );
+		node1.addSubtree( node3 );
+
+		Tree node4 = new Tree();
+		node4.addAttributeToId( ATTRIBUTE, 1 );
+		node2.addSubtree( node4 );
+
+		Tree node5 = new Tree();
+		node5.addAttributeToId( ATTRIBUTE, 1 );
+		node2.addSubtree( node5 );
+
+		return node1;
+	}
+
+	private Tree tree5()
+	{
+		//
+		//                 node1(node_weight=13)
+		//        ┌-─────────┴─────────────┐
+		//        │                        │
+		//      node2(node_weight=203)     node3(node_weight=203)
+
+		Tree node1 = new Tree();
+		node1.addAttributeToId( ATTRIBUTE, 13 );
+
+		Tree node2 = new Tree();
+		node2.addAttributeToId( ATTRIBUTE, 203 );
+		node1.addSubtree( node2 );
+
+		Tree node3 = new Tree();
+		node3.addAttributeToId( ATTRIBUTE, 203 );
+		node1.addSubtree( node3 );
+
+		return node1;
+	}
+
+	private Tree tree6()
+	{
+		//                 node1(node_weight=12)
+		//        ┌-─────────┴─────────────┐
+		//        │                        │
+		//      node2(node_weight=227)     node3(node_weight=227)
+
+		Tree node1 = new Tree();
+		node1.addAttributeToId( ATTRIBUTE, 12 );
+
+		Tree node2 = new Tree();
+		node2.addAttributeToId( ATTRIBUTE, 227 );
+		node1.addSubtree( node2 );
+
+		Tree node3 = new Tree();
+		node3.addAttributeToId( ATTRIBUTE, 227 );
+		node1.addSubtree( node3 );
+
+		return node1;
+	}
+
+	private Tree tree7()
+	{
+		//
+		//                 node1(node_weight=12)
+		//        ┌-─────────┴─────────────┐
+		//        │                        │
+		//      node2(node_weight=227)   node3(node_weight=227)
+		//                      ┌-─────────┴─────────────┐
+		//                    node4(node_weight=10)    node5(node_weight=10)
+
+		Tree node1 = new Tree();
+		node1.addAttributeToId( ATTRIBUTE, 12 );
+
+		Tree node2 = new Tree();
+		node2.addAttributeToId( ATTRIBUTE, 227 );
+		node1.addSubtree( node2 );
+
+		Tree node3 = new Tree();
+		node3.addAttributeToId( ATTRIBUTE, 227 );
+		node1.addSubtree( node3 );
+
+		Tree node4 = new Tree();
+		node4.addAttributeToId( ATTRIBUTE, 10 );
+		node3.addSubtree( node4 );
+
+		Tree node5 = new Tree();
+		node5.addAttributeToId( ATTRIBUTE, 10 );
+		node3.addSubtree( node5 );
 
 		return node1;
 	}
