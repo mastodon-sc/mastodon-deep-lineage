6508cfcee8be9e134f1c828702d55562a9d80df0
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-06-19 15:04:45 +0200

    Add null cost function tests for BranchSpotTree

--- src/test/java/org/mastodon/mamut/treesimilarity/ZhangUnorderedTreeEditDistanceTest.java
+++ src/test/java/org/mastodon/mamut/treesimilarity/ZhangUnorderedTreeEditDistanceTest.java
@@ -83,23 +83,23 @@ public class ZhangUnorderedTreeEditDistanceTest
 
 
 		// 60, because: 3 nodes with a total weight of 60 are added to the empty tree
-		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( simpleTree1, branchSpotEmptyTree, costFunction ), 0d );
-		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( branchSpotEmptyTree, simpleTree1, costFunction ), 0d );
+		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( simpleTree1, simpleEmptyTree, costFunction ), 0d );
+		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( simpleEmptyTree, simpleTree1, costFunction ), 0d );
 		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( branchSpotTree1, branchSpotEmptyTree, costFunction ), 0d );
 		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( branchSpotEmptyTree, branchSpotTree1, costFunction ), 0d );
 		// 60, because: 3 nodes with a total weight of 60 are added to the empty tree
-		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( simpleTree2, branchSpotEmptyTree, costFunction ), 0d );
-		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( branchSpotEmptyTree, simpleTree2, costFunction ), 0d );
+		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( simpleTree2, simpleEmptyTree, costFunction ), 0d );
+		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( simpleEmptyTree, simpleTree2, costFunction ), 0d );
 		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( branchSpotTree2, branchSpotEmptyTree, costFunction ), 0d );
 		assertEquals( 60d, ZhangUnorderedTreeEditDistance.distance( branchSpotEmptyTree, branchSpotTree2, costFunction ), 0d );
 		// 104, because: 3 nodes with a total weight of 104 are added to the empty tree
-		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( simpleTree3, branchSpotEmptyTree, costFunction ), 0d );
-		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( branchSpotEmptyTree, simpleTree3, costFunction ), 0d );
+		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( simpleTree3, simpleEmptyTree, costFunction ), 0d );
+		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( simpleEmptyTree, simpleTree3, costFunction ), 0d );
 		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( branchSpotTree3, branchSpotEmptyTree, costFunction ), 0d );
 		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( branchSpotEmptyTree, branchSpotTree3, costFunction ), 0d );
 		// 104, because: 3 nodes with a total weight of 104 are added to the empty tree
-		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( simpleTree4, branchSpotEmptyTree, costFunction ), 0d );
-		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( branchSpotEmptyTree, simpleTree4, costFunction ), 0d );
+		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( simpleTree4, simpleEmptyTree, costFunction ), 0d );
+		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( simpleEmptyTree, simpleTree4, costFunction ), 0d );
 		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( branchSpotTree4, branchSpotEmptyTree, costFunction ), 0d );
 		assertEquals( 104d, ZhangUnorderedTreeEditDistance.distance( branchSpotEmptyTree, branchSpotTree4, costFunction ), 0d );
 	}
@@ -108,29 +108,45 @@ public class ZhangUnorderedTreeEditDistanceTest
 	public void testDistanceNullCostFunction()
 	{
 
-		Tree< Double > tree1 = SimpleTreeExamples.tree1();
-		Tree< Double > tree2 = SimpleTreeExamples.tree2();
-		Tree< Double > tree3 = SimpleTreeExamples.tree3();
-		Tree< Double > tree4 = SimpleTreeExamples.tree4();
-		Tree< Double > tree5 = SimpleTreeExamples.tree5();
-		Tree< Double > tree6 = SimpleTreeExamples.tree6();
-		Tree< Double > tree7 = SimpleTreeExamples.tree7();
+		Tree< Double > simpleTree1 = SimpleTreeExamples.tree1();
+		Tree< Double > simpleTree2 = SimpleTreeExamples.tree2();
+		Tree< Double > simpleTree3 = SimpleTreeExamples.tree3();
+		Tree< Double > simpleTree4 = SimpleTreeExamples.tree4();
+		Tree< Double > simpleTree5 = SimpleTreeExamples.tree5();
+		Tree< Double > simpleTree6 = SimpleTreeExamples.tree6();
+		Tree< Double > simpleTree7 = SimpleTreeExamples.tree7();
+
+		Tree< Double > branchSpotTree1 = BranchSpotTreeExamples.tree1();
+		Tree< Double > branchSpotTree2 = BranchSpotTreeExamples.tree2();
+		Tree< Double > branchSpotTree3 = BranchSpotTreeExamples.tree3();
+		Tree< Double > branchSpotTree4 = BranchSpotTreeExamples.tree4();
+		Tree< Double > branchSpotTree5 = BranchSpotTreeExamples.tree5();
+		Tree< Double > branchSpotTree6 = BranchSpotTreeExamples.tree6();
+		Tree< Double > branchSpotTree7 = BranchSpotTreeExamples.tree7();
 
 
 		// 0, because: the trees are topologically identical
-		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( tree1, tree2, null ), 0d );
-		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( tree2, tree1, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( simpleTree1, simpleTree2, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( simpleTree2, simpleTree1, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( branchSpotTree1, branchSpotTree2, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( branchSpotTree2, branchSpotTree1, null ), 0d );
 		// 0, because: the trees are topologically identical
-		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( tree3, tree4, null ), 0d );
-		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( tree4, tree3, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( simpleTree3, simpleTree4, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( simpleTree4, simpleTree3, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( branchSpotTree3, branchSpotTree4, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( branchSpotTree4, branchSpotTree3, null ), 0d );
 
 		// 0, because: the trees are topologically identical
-		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( tree5, tree6, null ), 0d );
-		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( tree6, tree5, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( simpleTree5, simpleTree6, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( simpleTree6, simpleTree5, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( branchSpotTree5, branchSpotTree6, null ), 0d );
+		assertEquals( 0, ZhangUnorderedTreeEditDistance.distance( branchSpotTree6, branchSpotTree5, null ), 0d );
 
 		// 2, because: two extra nodes are added
-		assertEquals( 2, ZhangUnorderedTreeEditDistance.distance( tree5, tree7, null ), 0d );
-		assertEquals( 2, ZhangUnorderedTreeEditDistance.distance( tree7, tree5, null ), 0d );
+		assertEquals( 2, ZhangUnorderedTreeEditDistance.distance( simpleTree5, simpleTree7, null ), 0d );
+		assertEquals( 2, ZhangUnorderedTreeEditDistance.distance( simpleTree7, simpleTree5, null ), 0d );
+		assertEquals( 2, ZhangUnorderedTreeEditDistance.distance( branchSpotTree5, branchSpotTree7, null ), 0d );
+		assertEquals( 2, ZhangUnorderedTreeEditDistance.distance( branchSpotTree7, branchSpotTree5, null ), 0d );
 	}
 
 	private static BiFunction< Double, Double, Double > getCostFunction()
