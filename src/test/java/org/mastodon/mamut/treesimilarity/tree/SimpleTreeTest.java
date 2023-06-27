39b0b2269fd229a00ab9296786d15e3428044916
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-06-27 17:07:13 +0200

    Fix bug in testGetChildren()

--- src/test/java/org/mastodon/mamut/treesimilarity/tree/SimpleTreeTest.java
+++ src/test/java/org/mastodon/mamut/treesimilarity/tree/SimpleTreeTest.java
@@ -1,9 +1,10 @@
 package org.mastodon.mamut.treesimilarity.tree;
 
 import org.junit.Test;
-import org.mastodon.mamut.treesimilarity.tree.SimpleTree;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,9 +17,9 @@ public class SimpleTreeTest
 		SimpleTree< Number > tree = new SimpleTree<>( 0 );
 		SimpleTree< Number > child1 = new SimpleTree<>( 1 );
 		SimpleTree< Number > child2 = new SimpleTree<>( 2 );
-		tree.addSubtree( child1 );
-		tree.addSubtree( child2 );
-		Set< SimpleTree< Number > > expected = new HashSet( Arrays.asList( child1, child2 ) )
+		tree.addChild( child1 );
+		tree.addChild( child2 );
+		Set< SimpleTree< Number > > expected = new HashSet<>( Arrays.asList( child1, child2 ) );
 		assertEquals( expected, tree.getChildren() );
 	}
 
@@ -35,7 +36,7 @@ public class SimpleTreeTest
 	{
 		SimpleTree< Number > tree = new SimpleTree<>( 0 );
 		SimpleTree< Number > child1 = new SimpleTree<>( 1 );
-		tree.addSubtree( child1 );
+		tree.addChild( child1 );
 		assertEquals( child1, tree.getChildren().iterator().next() );
 	}
 
