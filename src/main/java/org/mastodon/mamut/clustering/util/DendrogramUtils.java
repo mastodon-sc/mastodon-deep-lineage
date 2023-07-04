38fe8d1eacdfd6434c56c67d590deda686ca58f2
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-07-04 23:48:21 +0200

    Add new class and unit test DendrogramUtils

---
+++ src/main/java/org/mastodon/mamut/clustering/util/DendrogramUtils.java
@@ -0,0 +1,64 @@
+package org.mastodon.mamut.clustering.util;
+
+import com.apporiented.algorithm.clustering.Cluster;
+
+import java.util.Map;
+
+public class DendrogramUtils
+{
+
+	private DendrogramUtils()
+	{
+		// prevent from instantiation
+	}
+
+	/**
+	 * Counts the number of zeros after the decimal point of the given number before the first non-zero digit.<p>
+	 * For numbers greater or equal to 1, 0 is returned.
+	 * E.g.
+	 * <ul>
+	 *     <li>5.01 -> 0</li>
+	 *     <li>0.1 -> 0</li>
+	 *     <li>0.01 -> 1</li>
+	 *     <li>0.001 -> 2</li>
+	 *
+	 * </ul>
+	 * @param number the number to count the zeros after the decimal point
+	 * @return the number of zeros after the decimal point of the given number before the first non-zero digit
+	 */
+	public static int countZerosAfterDecimalPoint( double number )
+	{
+		if ( number >= 1 )
+			return 0;
+		String numberString = String.valueOf( number );
+		int decimalIndex = numberString.indexOf( '.' );
+
+		int zeroCount = 0;
+		for ( int i = decimalIndex + 1; i < numberString.length(); i++ )
+		{
+			if ( numberString.charAt( i ) == '0' )
+				zeroCount++;
+			else
+				break;
+		}
+		return zeroCount;
+	}
+
+	/**
+	 * Renames the leaves of the given cluster according to the given object mapping..<p>
+	 * @param cluster the cluster to rename
+	 * @param objectMapping the mapping from cluster names to objects
+	 * @param <T> the type of the objects
+	 */
+	public static < T > void renameLeaves( final Cluster cluster, final Map< String, T > objectMapping )
+	{
+		if ( cluster == null )
+			throw new IllegalArgumentException( "Given cluster must not be null" );
+		if ( objectMapping == null )
+			throw new IllegalArgumentException( "Given objectMapping must not be null" );
+		if ( cluster.isLeaf() )
+			cluster.setName( objectMapping.get( cluster.getName() ).toString() );
+		for ( Cluster child : cluster.getChildren() )
+			renameLeaves( child, objectMapping );
+	}
+}
---
+++ src/test/java/org/mastodon/mamut/clustering/util/DendrogramUtilsTest.java
@@ -0,0 +1,43 @@
+package org.mastodon.mamut.clustering.util;
+
+import com.apporiented.algorithm.clustering.Cluster;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class DendrogramUtilsTest
+{
+
+	@Test
+	public void testCountZerosAfterDecimalPoint()
+	{
+		assertEquals( 0, DendrogramUtils.countZerosAfterDecimalPoint( 5 ) );
+		assertEquals( 0, DendrogramUtils.countZerosAfterDecimalPoint( 0.1 ) );
+		assertEquals( 1, DendrogramUtils.countZerosAfterDecimalPoint( 0.01 ) );
+		assertEquals( 2, DendrogramUtils.countZerosAfterDecimalPoint( -0.003 ) );
+	}
+
+	@Test
+	public void testRenameClusterLeaves()
+	{
+		Cluster cluster1 = new Cluster( "1" );
+		Cluster cluster2 = new Cluster( "2" );
+		Cluster cluster3 = new Cluster( "3" );
+		cluster1.addChild( cluster2 );
+		cluster1.addChild( cluster3 );
+
+		Map< String, String > objectMapping = new HashMap<>();
+		objectMapping.put( "1", "A" );
+		objectMapping.put( "2", "B" );
+		objectMapping.put( "3", "C" );
+
+		DendrogramUtils.renameLeaves( cluster1, objectMapping );
+
+		assertEquals( "1", cluster1.getName() ); // not a leaf -> not renamed
+		assertEquals( "B", cluster2.getName() );
+		assertEquals( "C", cluster3.getName() );
+	}
+}
