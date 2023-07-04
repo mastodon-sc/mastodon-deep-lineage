c6972b2abb554bc7474dc6b067b9a544051de0eb
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-07-04 11:16:38 +0200

    Add unit tests to cluster utils for exceptions and trivial cases

--- src/test/java/org/mastodon/mamut/clustering/util/ClusterUtilsTest.java
+++ src/test/java/org/mastodon/mamut/clustering/util/ClusterUtilsTest.java
@@ -1,6 +1,7 @@
 package org.mastodon.mamut.clustering.util;
 
 import com.apporiented.algorithm.clustering.AverageLinkageStrategy;
+import com.apporiented.algorithm.clustering.LinkageStrategy;
 import org.junit.Test;
 import org.mastodon.mamut.clustering.ClusterData;
 import org.mastodon.mamut.clustering.config.SimilarityMeasure;
@@ -12,6 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertThrows;
 
 public class ClusterUtilsTest
 {
@@ -71,5 +73,45 @@ public class ClusterUtilsTest
 		assertArrayEquals( new double[] { 0, t1t2, t1t3 }, distanceMatrix[ 0 ], 0d );
 		assertArrayEquals( new double[] { t1t2, 0, t2t3 }, distanceMatrix[ 1 ], 0d );
 		assertArrayEquals( new double[] { t1t3, t2t3, 0 }, distanceMatrix[ 2 ], 0d );
+	}
+
+	@Test
+	public void testExceptions()
+	{
+		LinkageStrategy linkageStrategy = new AverageLinkageStrategy();
+		// zero classes
+		assertThrows( IllegalArgumentException.class,
+				() -> ClusterUtils.getClustersByClassCount( ClusterData.names, ClusterData.fixedDistances, linkageStrategy, 0 ) );
+		// too many classes
+		assertThrows( IllegalArgumentException.class,
+				() -> ClusterUtils.getClustersByClassCount( ClusterData.names, ClusterData.fixedDistances, linkageStrategy, 11 ) );
+		// negative threshold
+		assertThrows( IllegalArgumentException.class,
+				() -> ClusterUtils.getClustersByThreshold( ClusterData.names, ClusterData.fixedDistances, linkageStrategy, -1 ) );
+	}
+
+	@Test
+	public void testOneClass()
+	{
+		Map< Integer, List< String > > leavesToClusterIds =
+				ClusterUtils.getClustersByClassCount( ClusterData.names, ClusterData.fixedDistances, new AverageLinkageStrategy(), 1 );
+		assertArrayEquals( new String[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" }, leavesToClusterIds.get( 0 ).toArray() );
+	}
+
+	@Test
+	public void testTrivialCase()
+	{
+		Map< Integer, List< String > > leavesToClusterIds =
+				ClusterUtils.getClustersByClassCount( ClusterData.names, ClusterData.fixedDistances, new AverageLinkageStrategy(), 10 );
+		assertArrayEquals( new String[] { "A" }, leavesToClusterIds.get( 0 ).toArray() );
+		assertArrayEquals( new String[] { "B" }, leavesToClusterIds.get( 1 ).toArray() );
+		assertArrayEquals( new String[] { "C" }, leavesToClusterIds.get( 2 ).toArray() );
+		assertArrayEquals( new String[] { "D" }, leavesToClusterIds.get( 3 ).toArray() );
+		assertArrayEquals( new String[] { "E" }, leavesToClusterIds.get( 4 ).toArray() );
+		assertArrayEquals( new String[] { "F" }, leavesToClusterIds.get( 5 ).toArray() );
+		assertArrayEquals( new String[] { "G" }, leavesToClusterIds.get( 6 ).toArray() );
+		assertArrayEquals( new String[] { "H" }, leavesToClusterIds.get( 7 ).toArray() );
+		assertArrayEquals( new String[] { "I" }, leavesToClusterIds.get( 8 ).toArray() );
+		assertArrayEquals( new String[] { "J" }, leavesToClusterIds.get( 9 ).toArray() );
 	}
 }
