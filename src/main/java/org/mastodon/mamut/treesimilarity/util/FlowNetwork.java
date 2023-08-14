1e47e45fbab1c0fade1f0962d8911ba84608b250
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-08-14 14:25:55 +0200

    Add javadoc to public methods of FlowNetwork

--- src/main/java/org/mastodon/mamut/treesimilarity/util/FlowNetwork.java
+++ src/main/java/org/mastodon/mamut/treesimilarity/util/FlowNetwork.java
@@ -8,6 +8,12 @@ import org.jgrapht.alg.interfaces.MinimumCostFlowAlgorithm;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
+/**
+ * A utility class that encapsulates a graph ({@link SimpleDirectedWeightedGraph}), a corresponding map of edge capacities and a minimum cost flow solution for this graph.<p>
+ *
+ * @author Matthias Arzt
+ * @author Stefan Hahmann
+ */
 public class FlowNetwork
 {
 
@@ -17,11 +23,22 @@ public class FlowNetwork
 
 	private MinimumCostFlowAlgorithm.MinimumCostFlow< DefaultWeightedEdge > flow;
 
+	/**
+	 * Adds a collection of vertices to the graph.
+	 * @param vertices vertices to add
+	 */
 	public void addVertices( Collection< ? > vertices )
 	{
 		vertices.forEach( graph::addVertex );
 	}
 
+	/**
+	 * Adds an edge to the graph with the given capacity and weight.
+	 * @param source source vertex
+	 * @param target target vertex
+	 * @param capacity capacity of the edge
+	 * @param weight weight of the edge
+	 */
 	public void addEdge( Object source, Object target, int capacity, double weight )
 	{
 		DefaultWeightedEdge e1 = graph.addEdge( source, target );
@@ -29,11 +46,23 @@ public class FlowNetwork
 		capacities.put( e1, capacity );
 	}
 
+	/**
+	 * Solves the maximum flow minimum cost problem on the graph for the given source and sink.
+	 * @param source source vertex
+	 * @param sink sink vertex
+	 */
 	public void solveMaxFlowMinCost( Object source, Object sink )
 	{
 		flow = JGraphtTools.maxFlowMinCost( graph, capacities, source, sink );
 	}
 
+	/**
+	 * Returns the flow on the edge from source to target.<p>
+	 * NB: The flow is only defined after {@link #solveMaxFlowMinCost(Object, Object)} has been called at least once.
+	 * @param source source vertex
+	 * @param target target vertex
+	 * @return the flow on the edge from source to target
+	 */
 	public double getFlow( Object source, Object target )
 	{
 		return flow.getFlow( graph.getEdge( source, target ) );
