4b4398ddcec9f616255f4553c30aae084b1a683b
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-08-21 17:00:15 +0200

    Rewrite LineageTreeUtils.callDepthFirst using generics

--- src/main/java/org/mastodon/mamut/util/LineageTreeUtils.java
+++ src/main/java/org/mastodon/mamut/util/LineageTreeUtils.java
@@ -1,13 +1,13 @@
 package org.mastodon.mamut.util;
 
 import org.mastodon.collection.RefSet;
+import org.mastodon.graph.Edge;
+import org.mastodon.graph.Graph;
+import org.mastodon.graph.Vertex;
 import org.mastodon.graph.algorithm.RootFinder;
 import org.mastodon.graph.algorithm.traversal.DepthFirstSearch;
 import org.mastodon.graph.algorithm.traversal.GraphSearch;
 import org.mastodon.graph.algorithm.traversal.SearchListener;
-import org.mastodon.mamut.model.branch.BranchLink;
-import org.mastodon.mamut.model.branch.BranchSpot;
-import org.mastodon.mamut.model.branch.ModelBranchGraph;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -22,45 +22,47 @@ public class LineageTreeUtils {
 	}
 
 	/**
-	 * Performs a depth-first iteration through the specified {@link ModelBranchGraph} starting from each root.
+	 * Performs a depth-first iteration through the specified {@link Graph} starting from each root.
 	 * <p>
 	 * The iteration is performed in a single thread. The given {@code action} is called for each vertex in the graph, when all its descendants in the search tree have been iterated through or when it is a leaf in the tree.
 	 *
-	 * @param branchGraph the graph to iterate through.
+	 * @param graph the graph to iterate through.
 	 * @param action      the action to perform on each vertex.
 	 * @param stopCondition optional condition that, when supplies true, stops the iteration before the next root is processed.
 	 */
-	public static void callDepthFirst( @Nonnull ModelBranchGraph branchGraph, @Nonnull Consumer< BranchSpot > action,
+	public static < V extends Vertex< E >, E extends Edge< V > > void callDepthFirst(
+			@Nonnull Graph< V, E > graph, @Nonnull Consumer< V > action,
 			@Nullable BooleanSupplier stopCondition )
 	{
-		DepthFirstSearch<BranchSpot, BranchLink> search = new DepthFirstSearch<>(branchGraph, GraphSearch.SearchDirection.DIRECTED);
-		search.setTraversalListener(new SearchListener<BranchSpot, BranchLink, DepthFirstSearch<BranchSpot, BranchLink>>() {
+		DepthFirstSearch< V, E > search = new DepthFirstSearch<>( graph, GraphSearch.SearchDirection.DIRECTED );
+		search.setTraversalListener( new SearchListener< V, E, DepthFirstSearch< V, E > >()
+		{
 			@Override
-			public void processVertexLate( BranchSpot vertex, DepthFirstSearch< BranchSpot, BranchLink > search )
+			public void processVertexLate( V vertex, DepthFirstSearch< V, E > search )
 			{
 				action.accept(vertex);
 			}
 
 			@Override
-			public void processVertexEarly( BranchSpot vertex, DepthFirstSearch< BranchSpot, BranchLink > search )
+			public void processVertexEarly( V vertex, DepthFirstSearch< V, E > search )
 			{
 				// Do nothing here. We only care about the vertices after all their descendants have been processed (see processVertexLate).
 			}
 
 			@Override
-			public void processEdge( BranchLink edge, BranchSpot from, BranchSpot to, DepthFirstSearch< BranchSpot, BranchLink > search )
+			public void processEdge( E edge, V from, V to, DepthFirstSearch< V, E > search )
 			{
 				// Do nothing here. We only care about the vertices after all their descendants have been processed (see processVertexLate).
 			}
 
 			@Override
-			public void crossComponent( BranchSpot from, BranchSpot to, DepthFirstSearch< BranchSpot, BranchLink > search )
+			public void crossComponent( V from, V to, DepthFirstSearch< V, E > search )
 			{
 				// Do nothing here. We only care about the vertices after all their descendants have been processed (see processVertexLate).
 			}
 		} );
-		final RefSet< BranchSpot > roots = RootFinder.getRoots( branchGraph );
-		for ( BranchSpot root : roots )
+		final RefSet< V > roots = RootFinder.getRoots( graph );
+		for ( V root : roots )
 		{
 			if ( stopCondition != null && stopCondition.getAsBoolean() )
 				break;
