675dc358b6abcc3be03485180863fda83eb48484
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-11-01 14:19:32 +0100

    Add new class CustomizedClusterComponent
    * Extends existing ClusterComponent with a Color specific to it
    * The color is set before the paint method of super class ClusterComponent is called

---
+++ src/main/java/org/mastodon/mamut/clustering/ui/CustomizedClusterComponent.java
@@ -0,0 +1,37 @@
+package org.mastodon.mamut.clustering.ui;
+
+import com.apporiented.algorithm.clustering.Cluster;
+import com.apporiented.algorithm.clustering.visualization.ClusterComponent;
+import com.apporiented.algorithm.clustering.visualization.VCoord;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class CustomizedClusterComponent extends ClusterComponent
+{
+	private final Color color;
+
+	public CustomizedClusterComponent( final Cluster cluster, final boolean printName, final VCoord initPoint, final Color color )
+	{
+		super( cluster, printName, initPoint );
+		this.color = color;
+	}
+
+	@Override
+	public void paint(
+			final Graphics2D g2, final int xDisplayOffset, final int yDisplayOffset, final double xDisplayFactor,
+			final double yDisplayFactor, final boolean decorated
+	)
+	{
+		Color defaultColor = g2.getColor();
+		try
+		{
+			g2.setColor( color );
+			super.paint( g2, xDisplayOffset, yDisplayOffset, xDisplayFactor, yDisplayFactor, decorated );
+		}
+		finally
+		{
+			g2.setColor( defaultColor );
+		}
+	}
+}
