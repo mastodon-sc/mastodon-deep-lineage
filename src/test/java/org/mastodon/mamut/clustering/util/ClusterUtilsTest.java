51b2a90440eb2f4f7eedec86a6ef76cc816118dc
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-11-02 13:22:56 +0100

    Remove unrequired import from ClusterUtilsTest

--- src/test/java/org/mastodon/mamut/clustering/util/ClusterUtilsTest.java
+++ src/test/java/org/mastodon/mamut/clustering/util/ClusterUtilsTest.java
@@ -14,7 +14,6 @@ import org.mastodon.util.ColorUtils;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
