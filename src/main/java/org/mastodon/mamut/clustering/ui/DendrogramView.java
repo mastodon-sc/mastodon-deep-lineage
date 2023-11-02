34524f782744a7978b773c1ffec763a9576e5947
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-11-02 14:40:48 +0100

    Remove DendrogramPanelWithCutoffLine from DendrogramView and adaptScalebar
    * Painting the cutoff line and adapting the scalebar is now completely done in the DendrogramPanel

--- src/main/java/org/mastodon/mamut/clustering/ui/DendrogramView.java
+++ src/main/java/org/mastodon/mamut/clustering/ui/DendrogramView.java
@@ -47,9 +47,6 @@ public class DendrogramView< T >
 					classification.getCutoff(), classification.getLeafMapping()
 			);
 			dendrogramPanel.setBackground( Color.WHITE );
-			dendrogramPanel.setLineColor( Color.BLACK );
-			if ( cluster.getDistanceValue() <= 1d )
-				adaptScaleBar( dendrogramPanel );
 		}
 
 		JPanel panel = new JPanel( new MigLayout( "fill" ) );
@@ -61,62 +58,5 @@ public class DendrogramView< T >
 		panel.setBorder( BorderFactory.createEtchedBorder() );
 
 		return panel;
-	}
-
-	private void adaptScaleBar( DendrogramPanel dendrogramPanel )
-	{
-		if ( cluster == null )
-			return;
-		int zeros = DendrogramUtils.countZerosAfterDecimalPoint( cluster.getDistanceValue() );
-		dendrogramPanel.setScaleValueInterval( Math.pow( 10, -( zeros + 1 ) ) );
-		dendrogramPanel.setScaleValueDecimals( zeros + 1 );
-	}
-
-	private class DendrogramPanelWithCutoffLine extends DendrogramPanel
-	{
-		private final double maxValue;
-
-		private final double cutoff;
-
-		private DendrogramPanelWithCutoffLine( double maxValue, double cutoff )
-		{
-			super();
-			this.maxValue = maxValue;
-			this.cutoff = cutoff;
-		}
-
-		@Override
-		public void paint( Graphics g )
-		{
-			super.paint( g );
-			if ( cutoff == 0d )
-				return;
-			if ( !( g instanceof Graphics2D ) )
-				return;
-			if ( cluster == null )
-				return;
-
-			Graphics2D g2 = ( Graphics2D ) g;
-			Stroke defaultStroke = g2.getStroke();
-
-			try
-			{
-				Stroke stroke = new BasicStroke( 2, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[] { 5, 5 }, 0 );
-				g2.setStroke( stroke );
-				int yDendrogramOrigin = getBorderBottom() + getBorderTop();
-				int yDendrogramEnd = getHeight() - getBorderBottom();
-				ClusterComponent component = new ClusterComponent( cluster, false, new VCoord( 0, 0 ) );
-				int maxLabelWidth = DendrogramUtils.getMaxLeafNameWidth( g2, cluster ) + component.getNamePadding();
-				int screenWidth = getWidth() - getBorderLeft() - getBorderRight() - maxLabelWidth;
-				double xFactor = screenWidth / maxValue;
-				int xOffset = getBorderLeft();
-				int cutoffX = xOffset + ( int ) ( ( maxValue - cutoff ) * xFactor );
-				g.drawLine( cutoffX, yDendrogramOrigin, cutoffX, yDendrogramEnd );
-			}
-			finally
-			{
-				g2.setStroke( defaultStroke );
-			}
-		}
 	}
 }
