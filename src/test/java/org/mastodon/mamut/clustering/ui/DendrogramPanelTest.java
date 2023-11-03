4df963674b121298b6f5e4c21c72992887967f33
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-11-03 11:13:08 +0100

    Add basic test to check if a DendrogramPanel can be generated with a classification object

--- src/test/java/org/mastodon/mamut/clustering/ui/DendrogramPanelTest.java
+++ src/test/java/org/mastodon/mamut/clustering/ui/DendrogramPanelTest.java
@@ -1,8 +1,16 @@
 package org.mastodon.mamut.clustering.ui;
 
+import com.apporiented.algorithm.clustering.AverageLinkageStrategy;
+import com.apporiented.algorithm.clustering.Cluster;
+import com.apporiented.algorithm.clustering.ClusteringAlgorithm;
+import com.apporiented.algorithm.clustering.DefaultClusteringAlgorithm;
 import org.junit.Test;
+import org.mastodon.mamut.clustering.ClusterData;
+import org.mastodon.mamut.clustering.util.Classification;
+import org.mastodon.mamut.clustering.util.ClusterUtils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class DendrogramPanelTest
 {
@@ -13,5 +21,16 @@ public class DendrogramPanelTest
 		assertEquals( 0, DendrogramPanel.countZerosAfterDecimalPoint( 0.1 ) );
 		assertEquals( 1, DendrogramPanel.countZerosAfterDecimalPoint( 0.01 ) );
 		assertEquals( 2, DendrogramPanel.countZerosAfterDecimalPoint( -0.003 ) );
+	}
+
+	@Test
+	public void testDendrogramPanel()
+	{
+		Classification< String > classification =
+				ClusterUtils.getClassificationByClassCount( ClusterData.names, ClusterData.fixedDistances, new AverageLinkageStrategy(),
+						3
+				);
+		DendrogramPanel< String > dendrogramPanel = new DendrogramPanel<>( classification );
+		assertNotNull( dendrogramPanel );
 	}
 }
