6d272c9287216a80f6b40b3eb99024a8bd82c2c7
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2023-12-11 11:52:54 +0100

    Add <p> instead of an empty line in javadoc

--- src/main/java/org/mastodon/mamut/clustering/util/AverageLinkageUPGMAStrategy.java
+++ src/main/java/org/mastodon/mamut/clustering/util/AverageLinkageUPGMAStrategy.java
@@ -34,7 +34,7 @@ import com.apporiented.algorithm.clustering.WeightedLinkageStrategy;
  * UPGMA (unweighted pair group method with arithmetic mean) is a simple agglomerative (bottom-up) hierarchical clustering method.
  *
  * @see <a href="https://en.wikipedia.org/wiki/UPGMA">UPGMA</a>
- *
+ * <p>
  * This class extends the class {@link WeightedLinkageStrategy} from the hierarchical clustering library, since the naming of that class was misleading.
  */
 public class AverageLinkageUPGMAStrategy extends WeightedLinkageStrategy
