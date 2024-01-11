11ca0bfcd4c970efce043f61a01661c690c2e7a5
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-01-11 14:50:39 +0100

    Add unit tests for BranchAverageMovementFeature

--- src/test/java/org/mastodon/mamut/feature/branch/movement/BranchAverageMovementFeatureComputerTest.java
+++ src/test/java/org/mastodon/mamut/feature/branch/movement/BranchAverageMovementFeatureComputerTest.java
@@ -28,7 +28,51 @@
  */
 package org.mastodon.mamut.feature.branch.movement;
 
+import org.junit.Test;
+import org.mastodon.feature.FeatureProjection;
+import org.mastodon.mamut.feature.FeatureComputerTestUtils;
+import org.mastodon.mamut.feature.branch.exampleGraph.ExampleGraph1;
+import org.mastodon.mamut.feature.branch.exampleGraph.ExampleGraph2;
+import org.mastodon.mamut.model.branch.BranchSpot;
+import org.scijava.Context;
+
+import static org.junit.Assert.assertEquals;
+
 public class BranchAverageMovementFeatureComputerTest
 {
+	@Test
+	public void testCompute1()
+	{
+		try (Context context = new Context())
+		{
+			ExampleGraph1 exampleGraph1 = new ExampleGraph1();
+			FeatureProjection< BranchSpot > featureProjection =
+					FeatureComputerTestUtils.getFeatureProjection( context, exampleGraph1.getModel(),
+							BranchAverageMovementFeature.BRANCH_AVERAGE_MOVEMENT_FEATURE_SPEC,
+							BranchAverageMovementFeature.PROJECTION_SPEC );
+			double expected = 4 * Math.sqrt( 1 + 4 + 9 ) / 3;
+			assertEquals( expected, featureProjection.value( exampleGraph1.branchSpotA ), 0 );
+		}
+	}
 
+	@Test
+	public void testCompute2()
+	{
+		try (Context context = new Context())
+		{
+			ExampleGraph2 exampleGraph2 = new ExampleGraph2();
+			FeatureProjection< BranchSpot > featureProjection =
+					FeatureComputerTestUtils.getFeatureProjection( context, exampleGraph2.getModel(),
+							BranchAverageMovementFeature.BRANCH_AVERAGE_MOVEMENT_FEATURE_SPEC,
+							BranchAverageMovementFeature.PROJECTION_SPEC );
+
+			assertEquals( ( Math.sqrt( 1 + 4 + 9 ) + Math.sqrt( 9 + 36 + 81 ) ) / 2d,
+					featureProjection.value( exampleGraph2.branchSpotA ), 0 );
+			assertEquals( Math.sqrt( 4 + 16 + 36 ) / 2d, featureProjection.value( exampleGraph2.branchSpotC ), 0 );
+			assertEquals( Math.sqrt( 1 + 4 + 9 ), featureProjection.value( exampleGraph2.branchSpotB ), 0 );
+			assertEquals( ( Math.sqrt( 36 + 144 + 324 ) + Math.sqrt( 64 + 256 + 576 ) ) / 2d,
+					featureProjection.value( exampleGraph2.branchSpotD ), 0 );
+			assertEquals( Math.sqrt( 4 + 16 + 36 ) / 2d, featureProjection.value( exampleGraph2.branchSpotE ), 0 );
+		}
+	}
 }
