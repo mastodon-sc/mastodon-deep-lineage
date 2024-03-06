c24ded9b296f5d26b5f9071efd7d8b957b062944
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-03-06 14:36:28 +0100

    Add a new utility class that contains methods to create Spots from Label images
    * LabelImageUtilsTest contains unit tests for this class

---
+++ src/main/java/org/mastodon/mamut/io/importer/labelimage/LabelImageUtils.java
@@ -0,0 +1,373 @@
+package org.mastodon.mamut.io.importer.labelimage;
+
+import bdv.viewer.Source;
+import mpicbg.spim.data.generic.sequence.AbstractSequenceDescription;
+import mpicbg.spim.data.sequence.TimePoint;
+import mpicbg.spim.data.sequence.VoxelDimensions;
+import net.imagej.ImgPlus;
+import net.imglib2.Cursor;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Cast;
+import net.imglib2.util.Pair;
+import net.imglib2.util.ValuePair;
+import net.imglib2.view.Views;
+import org.mastodon.mamut.ProjectModel;
+import org.mastodon.mamut.io.importer.labelimage.math.CovarianceMatrix;
+import org.mastodon.mamut.io.importer.labelimage.math.MeansVector;
+import org.mastodon.mamut.model.Model;
+import org.mastodon.mamut.model.ModelGraph;
+import org.mastodon.mamut.model.Spot;
+import org.mastodon.views.bdv.SharedBigDataViewerData;
+import org.scijava.Context;
+import org.scijava.app.StatusService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.IntFunction;
+
+/**
+ * Utility class to create spots from label images.
+ */
+public class LabelImageUtils
+{
+
+	private static final Logger logger = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() );
+
+	private LabelImageUtils()
+	{
+		// prevent from instantiation
+	}
+
+	/**
+	 * Creates spots from the given label image.<br>
+	 * The method runs twice through each image (i.e. each frame). Once to determine maximum/minimum values for array initialization, and once to do summation for covariance and mean.
+	 * @param frameProvider a function that provides the image data for each frame.
+	 * @param frames a list of frames to process.
+	 * @param model the model to add the spots to.
+	 * @param voxelDimensions the dimensions of the voxels in the image.
+	 * @param sigma the standard deviation of the Gaussian distribution to use for the covariance matrix.
+	 * @param statusService the status service to report progress to.
+	 */
+	static void createSpotsFromLabelImage( final IntFunction< RandomAccessibleInterval< RealType< ? > > > frameProvider,
+			final List< TimePoint > frames, final Model model, final VoxelDimensions voxelDimensions, final double sigma,
+			final StatusService statusService )
+	{
+		final ModelGraph graph = model.getGraph();
+		ReentrantReadWriteLock lock = graph.getLock();
+		lock.writeLock().lock();
+		int count = 0;
+		try
+		{
+			int numTimepoints = frames.size();
+			for ( TimePoint frame : frames )
+			{
+				int frameId = frame.getId();
+				final RandomAccessibleInterval< RealType< ? > > rai = frameProvider.apply( frameId );
+				count += createSpotsForFrame( graph, rai, frameId, voxelDimensions, sigma );
+				if ( statusService != null )
+					statusService.showProgress( frameId + 1, numTimepoints );
+			}
+			model.setUndoPoint();
+		}
+		finally
+		{
+			lock.writeLock().unlock();
+		}
+		graph.notifyGraphChanged();
+		logger.info( "Created {} new spots in {} frames.", count, frames.size() );
+	}
+
+	/**
+	 * Create spots for the given frame.
+	 * @param graph the graph to add the spots to.
+	 * @param frame the image data to read and process.
+	 * @param frameId the frame id the spots should belong to.
+	 * @param voxelDimensions the dimensions of the voxels in the image.
+	 * @param sigma the standard deviation of the Gaussian distribution to use for the covariance matrix.
+	 * @return the number of spots created.
+	 */
+	private static int createSpotsForFrame( final ModelGraph graph, final RandomAccessibleInterval< RealType< ? > > frame,
+			final int frameId, final VoxelDimensions voxelDimensions, final double sigma )
+	{
+		logger.debug( "Computing mean, covariance of all labels at frame {}", frameId );
+		logger.debug( "Dimensions of frame: {}, {}, {}", frame.dimension( 0 ), frame.dimension( 1 ), frame.dimension( 2 ) );
+
+		// get the maximum value possible to learn how many objects need to be instantiated
+		Pair< Integer, Integer > minAndMax = getPixelValueInterval( frame );
+
+		int minimumLabel = minAndMax.getA();
+		int maximumLabel = minAndMax.getB();
+		if ( minimumLabel == Integer.MAX_VALUE || maximumLabel == Integer.MIN_VALUE )
+		{
+			logger.debug( "No labels found in frame {}", frameId );
+			return 0;
+		}
+		int numLabels = maximumLabel - minimumLabel + 1;
+		if ( numLabels > 10_000 ) // NB: 10_000 is arbitrary, but such a high number of labels is suspicious, thus we log a warning here
+			logger.warn( "found {} labels, are you sure you used the correct image source?", numLabels );
+
+		logger.debug( "Found {} label(s) in frame {}. Range: [{}, {}]", numLabels, frameId, minimumLabel, maximumLabel );
+		Label[] labels = extractLabelsFromFrame( frame, minimumLabel, numLabels );
+		return createSpotsFromFrameLabels( graph, frameId, labels, voxelDimensions, sigma );
+	}
+
+	/**
+	 * Read the frame and determine the minimum and maximum pixel values in the frame.
+	 * @param frame an image to read and process
+	 * @return A pair of values (min, max) that represent the minimum and maximum pixel values in the image
+	 * @author Noam Dori
+	 */
+	private static Pair< Integer, Integer > getPixelValueInterval( final RandomAccessibleInterval< RealType< ? > > frame )
+	{
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		Cursor< RealType< ? > > cursor = Views.iterable( frame ).cursor();
+		while ( cursor.hasNext() )
+		{
+			int val = ( int ) cursor.next().getRealDouble();
+			// we ignore 0 as it is background
+			if ( val == 0 )
+				continue;
+			if ( min > val )
+				min = val;
+			if ( max < val )
+				max = val;
+		}
+		return new ValuePair<>( min, max );
+	}
+
+	/**
+	 * Create spots from the labels in the given frame.
+	 * @param graph the graph to add the spots to.
+	 * @param frameId the frame id the spots should belong to.
+	 * @param labels the labels in the frame.
+	 * @param voxelDimensions the dimensions of the voxels in the image.
+	 * @param sigma the standard deviation of the Gaussian distribution to use for the covariance matrix.
+	 * @return the number of spots created.
+	 */
+	private static int createSpotsFromFrameLabels( final ModelGraph graph, final int frameId, final Label[] labels,
+			final VoxelDimensions voxelDimensions, final double sigma )
+	{
+		int count = 0;
+		// combine the sums into mean and covariance matrices, then add the corresponding spot
+		for ( final Label label : labels )
+		{
+			// skip labels that are not present in the image or have only one pixel
+			if ( label == null || label.numPixels < 2 )
+				continue;
+			double[] mean = label.means.get();
+			double[][] cov = label.covariances.get();
+			scale( mean, voxelDimensions );
+			scale( cov, sigma, voxelDimensions );
+			try
+			{
+				Spot spot = graph.addVertex().init( frameId, mean, cov );
+				spot.setLabel( String.valueOf( label.value ) );
+				count++;
+			}
+			catch ( Exception e )
+			{
+				logger.trace( "Could not add vertex to graph. Mean: {}, Covariance: {}", Arrays.toString( mean ),
+						Arrays.deepToString( cov ) );
+			}
+		}
+		logger.debug( "Added {} spots to frame {}", count, frameId );
+		return count;
+	}
+
+	/**
+	 * Extracts the labels from the given frame by iterating once over all pixels of that frame and computing the means and covariances for all labels.
+	 * @param frame the pointer to the image to read.
+	 * @param minimumLabelValue the minimum value of the pixels in the image.
+	 * @param numLabels the number of labels in the frame.
+	 */
+	private static Label[] extractLabelsFromFrame( final RandomAccessibleInterval< RealType< ? > > frame, int minimumLabelValue,
+			int numLabels )
+	{
+		Label[] labels = new Label[ numLabels ];
+		// read all pixels of the picture to sum everything up
+		Cursor< RealType< ? > > cursor = Views.iterable( frame ).cursor();
+		int[] pixel = new int[ cursor.numDimensions() ];
+		while ( cursor.hasNext() )
+		{
+			int pixelValue = ( int ) cursor.next().getRealDouble();
+			int labelIndex = pixelValue - minimumLabelValue;
+			if ( pixelValue <= 0 )
+				continue;
+			if ( labelIndex >= numLabels )
+				throw new IllegalArgumentException(
+						"Index " + labelIndex + " is out of bounds for the number of  " + numLabels + " labels in this frame." );
+			if ( labels[ labelIndex ] == null )
+				labels[ labelIndex ] = new Label( pixelValue, cursor.numDimensions() );
+			Label label = labels[ labelIndex ];
+			cursor.localize( pixel );
+			label.addPixel( pixel );
+		}
+		return labels;
+	}
+
+	/**
+	 * Scales the mean vector by the voxel dimensions.
+	 * @param mean the mean vector to scale.
+	 * @param voxelDimensions the dimensions of the voxels in the image.
+	 */
+	private static void scale( final double[] mean, final VoxelDimensions voxelDimensions )
+	{
+		if ( mean.length != voxelDimensions.numDimensions() )
+			throw new IllegalArgumentException( "Mean vector has wrong dimension." );
+		for ( int i = 0; i < mean.length; i++ )
+			mean[ i ] = mean[ i ] * voxelDimensions.dimension( i );
+	}
+
+	/**
+	 * Returns the dimensions of the given image as an array in the order x, y, z, t.
+	 * @param imgPlus the image to get the dimensions from.
+	 * @return the dimensions of the image.
+	 */
+	public static long[] getImgPlusDimensions( final ImgPlus< ? > imgPlus )
+	{
+		long[] imgPlusDimensions = imgPlus.getImg().dimensionsAsLongArray();
+		int numDimensions = imgPlusDimensions.length;
+		long imgPlusX = imgPlusDimensions[ 0 ];
+		long imgPlusY = imgPlusDimensions[ 1 ];
+		long imgPlusZ = numDimensions > 2 ? imgPlusDimensions[ 2 ] : 0;
+		long imgPlusT = numDimensions > 3 ? imgPlusDimensions[ 3 ] : 0;
+		return new long[] { imgPlusX, imgPlusY, imgPlusZ, imgPlusT };
+	}
+
+	/**
+	 * Returns the dimensions of the given big data viewer image as an array in the order x, y, z, t.
+	 * @param sharedBdvData the big data viewer data to get the dimensions from.
+	 * @return the dimensions of the big data viewer image.
+	 */
+	public static long[] getBdvDimensions( final SharedBigDataViewerData sharedBdvData )
+	{
+		AbstractSequenceDescription< ?, ?, ? > sequenceDescription = sharedBdvData.getSpimData().getSequenceDescription();
+		int bdvTimepoints = sequenceDescription.getTimePoints().size();
+		long[] bdvSpatialDimensions = sequenceDescription.getViewSetups().get( 0 ).getSize().dimensionsAsLongArray();
+		return new long[] { bdvSpatialDimensions[ 0 ], bdvSpatialDimensions[ 1 ], bdvSpatialDimensions[ 2 ], bdvTimepoints };
+	}
+
+	/**
+	 * Checks if the dimensions of the given big data viewer image match the dimensions of the given image.
+	 * @param sharedBigDataViewerData the big data viewer data to check the dimensions against.
+	 * @param imgPlus the image to check the dimensions against.
+	 * @return true if the dimensions match, false otherwise.
+	 */
+	public static boolean dimensionsMatch( final SharedBigDataViewerData sharedBigDataViewerData, final ImgPlus< ? > imgPlus )
+	{
+		long[] bdvDimensions = getBdvDimensions( sharedBigDataViewerData );
+		long[] imgPlusDimensions = getImgPlusDimensions( imgPlus );
+		return Arrays.equals( bdvDimensions, imgPlusDimensions );
+	}
+
+	/**
+	 * Imports spots from the given ImageJ image into the given project model.
+	 * @param imgPlus the image to import the spots from.
+	 * @param sigma the standard deviation of the Gaussian distribution to use for the covariance matrix.
+	 * @param projectModel the project model to add the spots to.
+	 * @throws IllegalArgumentException if the dimensions of the given image do not match the dimensions of the big data viewer image contained in the project model.
+	 */
+	public static void importSpotsFromImgPlus( final ImgPlus< ? > imgPlus, final double sigma, final ProjectModel projectModel )
+	{
+		logger.debug( "ImageJ image: {}", imgPlus.getName() );
+		final SharedBigDataViewerData sharedBdvData = projectModel.getSharedBdvData();
+		if ( !dimensionsMatch( sharedBdvData, imgPlus ) )
+			throw new IllegalArgumentException( "The dimensions of the ImageJ image " + imgPlus.getName()
+					+ " do not match the dimensions of the big data viewer image." );
+		final Context context = projectModel.getContext();
+		final AbstractSequenceDescription< ?, ?, ? > sequenceDescription = sharedBdvData.getSpimData().getSequenceDescription();
+		final List< TimePoint > frames = sequenceDescription.getTimePoints().getTimePointsOrdered();
+		// NB: Use the dimensions of the first source and the first time point only without checking if they are equal in other sources and time points.
+		final VoxelDimensions voxelDimensions = sequenceDescription.getViewSetups().get( 0 ).getVoxelSize();
+		IntFunction< RandomAccessibleInterval< RealType< ? > > > frameProvider =
+				frameId -> Cast.unchecked( Views.hyperSlice( imgPlus.getImg(), 3, frameId ) );
+		LabelImageUtils.createSpotsFromLabelImage( frameProvider, frames, projectModel.getModel(), voxelDimensions, sigma,
+				context.getService( StatusService.class ) );
+	}
+
+	/**
+	 * Imports spots from the given big data viewer channel into the given project model.
+	 * @param projectModel the project model to add the spots to.
+	 * @param labelChannelIndex the index of the channel to import the spots from. This channel should contain the labels.
+	 * @param sigma the standard deviation of the Gaussian distribution to use for the covariance matrix.
+	 * @throws IllegalArgumentException if the label channel index is out of bounds, i.e. if it is greater than the number of channels in the big data viewer source contained in the project model.
+	 */
+	public static void importSpotsFromBdvChannel( final ProjectModel projectModel, final int labelChannelIndex, final double sigma )
+	{
+		final SharedBigDataViewerData sharedBigDataViewerData = projectModel.getSharedBdvData();
+		int numChannels = sharedBigDataViewerData.getSources().size();
+		if ( labelChannelIndex >= numChannels )
+			throw new IllegalArgumentException( "The label channel index " + labelChannelIndex
+					+ " is out of bounds. The available big data viewer source only contains " + numChannels + " channels." );
+		final AbstractSequenceDescription< ?, ?, ? > sequenceDescription = sharedBigDataViewerData.getSpimData().getSequenceDescription();
+		final List< TimePoint > frames = sequenceDescription.getTimePoints().getTimePointsOrdered();
+		final Source< ? extends RealType< ? > > source =
+				Cast.unchecked( sharedBigDataViewerData.getSources().get( labelChannelIndex ).getSpimSource() );
+		// NB: Use the dimensions of the first source and the first time point only without checking if they are equal in other sources and time points.
+		final VoxelDimensions voxelDimensions = sequenceDescription.getViewSetups().get( 0 ).getVoxelSize();
+		IntFunction< RandomAccessibleInterval< RealType< ? > > > imgProvider =
+				frameId -> Cast.unchecked( source.getSource( frameId, 0 ) );
+		LabelImageUtils.createSpotsFromLabelImage( imgProvider, frames, projectModel.getModel(), voxelDimensions, sigma,
+				projectModel.getContext().getService( StatusService.class ) );
+	}
+
+	/**
+	 * Scales the covariance matrix the given sigma and the voxel dimensions.
+	 * @param covariance the covariance matrix to scale.
+	 * @param sigma the standard deviation of the Gaussian distribution to use for the covariance matrix.
+	 * @param voxelDimensions the dimensions of the voxels in the image.
+	 * @throws IllegalArgumentException if the covariance matrix has not the same dimensions as the given voxelDimensions.
+	 */
+	public static void scale( final double[][] covariance, final double sigma, final VoxelDimensions voxelDimensions )
+	{
+		if ( covariance.length != voxelDimensions.numDimensions() )
+			throw new IllegalArgumentException( "Covariance matrix has wrong dimension." );
+		for ( int i = 0; i < covariance.length; i++ )
+		{
+			if ( covariance[ i ].length != voxelDimensions.numDimensions() )
+				throw new IllegalArgumentException( "Covariance matrix has wrong dimension." );
+			for ( int j = i; j < covariance.length; j++ )
+			{
+				covariance[ i ][ j ] *= Math.pow( sigma, 2 ) * voxelDimensions.dimension( i ) * voxelDimensions.dimension( j );
+				// the covariance matrix is symmetric!
+				if ( i != j )
+					covariance[ j ][ i ] = covariance[ i ][ j ];
+			}
+		}
+	}
+
+	/**
+	 * A class to hold the pixel count, mean and covariances of a label in the image.
+	 */
+	private static class Label
+	{
+		private final int value;
+
+		private int numPixels;
+
+		private final MeansVector means;
+
+		private final CovarianceMatrix covariances;
+
+		private Label( final int value, final int numDimensions )
+		{
+			this.value = value;
+			numPixels = 0;
+			means = new MeansVector( numDimensions );
+			covariances = new CovarianceMatrix( numDimensions );
+		}
+
+		private void addPixel( int[] pixel )
+		{
+			numPixels++;
+			means.addValues( pixel );
+			covariances.addValues( pixel );
+		}
+	}
+}
---
+++ src/test/java/org/mastodon/mamut/io/importer/labelimage/LabelImageUtilsTest.java
@@ -0,0 +1,250 @@
+package org.mastodon.mamut.io.importer.labelimage;
+
+import bdv.util.AbstractSource;
+import bdv.util.RandomAccessibleIntervalSource;
+import mpicbg.spim.data.sequence.FinalVoxelDimensions;
+import mpicbg.spim.data.sequence.TimePoint;
+import mpicbg.spim.data.sequence.VoxelDimensions;
+import net.imagej.ImgPlus;
+import net.imagej.axis.Axes;
+import net.imagej.axis.CalibratedAxis;
+import net.imagej.axis.DefaultLinearAxis;
+import net.imagej.patcher.LegacyInjector;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.loops.LoopBuilder;
+import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.util.Cast;
+import net.imglib2.view.Views;
+import org.junit.Before;
+import org.junit.Test;
+import org.mastodon.mamut.ProjectModel;
+import org.mastodon.mamut.feature.EllipsoidIterable;
+import org.mastodon.mamut.io.importer.labelimage.demo.DemoUtils;
+import org.mastodon.mamut.model.Model;
+import org.mastodon.mamut.model.Spot;
+import org.mastodon.views.bdv.overlay.util.JamaEigenvalueDecomposition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntFunction;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+public class LabelImageUtilsTest
+{
+	private static final Logger logger = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() );
+
+	private Model model;
+
+	private List< TimePoint > frames;
+
+	private VoxelDimensions voxelDimensions;
+
+	@Before
+	public void setUp()
+	{
+		model = new Model();
+		frames = Collections.singletonList( new TimePoint( 0 ) );
+		voxelDimensions = new FinalVoxelDimensions( "um", 1, 1, 1 );
+	}
+
+	@Test
+	public void testScaleExceptions()
+	{
+		VoxelDimensions voxelDimensions = new FinalVoxelDimensions( "um", 1, 1, 1 );
+		assertThrows( IllegalArgumentException.class, () -> LabelImageUtils.scale( new double[ 2 ][ 2 ], 1, voxelDimensions ) );
+		assertThrows( IllegalArgumentException.class, () -> LabelImageUtils.scale( new double[ 3 ][ 2 ], 1, voxelDimensions ) );
+	}
+
+	@Test
+	public void testCreateSpotFromLabelImageEmpty()
+	{
+		RandomAccessibleIntervalSource< FloatType > img =
+				new RandomAccessibleIntervalSource<>( createImageCubeCorners( 0 ), new FloatType(), new AffineTransform3D(),
+						"Segmentation" );
+
+		IntFunction< RandomAccessibleInterval< RealType< ? > > > imgProvider = frameId -> Cast.unchecked( img.getSource( frameId, 0 ) );
+		LabelImageUtils.createSpotsFromLabelImage( imgProvider, frames, model, voxelDimensions, 1, null );
+		assertEquals( 0, model.getGraph().vertices().size() );
+	}
+
+	@Test
+	public void testCreateSpotFromNonLabelImage()
+	{
+		AbstractSource< FloatType > img = createNonLabelImage();
+
+		IntFunction< RandomAccessibleInterval< RealType< ? > > > imgProvider = frameId -> Cast.unchecked( img.getSource( frameId, 0 ) );
+		LabelImageUtils.createSpotsFromLabelImage( imgProvider, frames, model, voxelDimensions, 1, null );
+		assertEquals( 0, model.getGraph().vertices().size() );
+	}
+
+	@Test
+	public void testCreateSpotFromWrongVoxelDimensions()
+	{
+		RandomAccessibleIntervalSource< FloatType > img =
+				new RandomAccessibleIntervalSource<>( createImageCubeCorners( 1 ), new FloatType(), new AffineTransform3D(),
+						"Segmentation" );
+
+		VoxelDimensions wrongDimensions = new FinalVoxelDimensions( "um", 1, 1 );
+		IntFunction< RandomAccessibleInterval< RealType< ? > > > imgProvider = frameId -> Cast.unchecked( img.getSource( frameId, 0 ) );
+		assertThrows( IllegalArgumentException.class,
+				() -> LabelImageUtils.createSpotsFromLabelImage( imgProvider, frames, model, wrongDimensions, 1, null ) );
+	}
+
+	@Test
+	public void testImportSpotsFromBdvChannel()
+	{
+		LegacyInjector.preinit();
+		int pixelValue = 1;
+		Img< FloatType > img = createImageCubeCorners( pixelValue );
+		ProjectModel projectModel = DemoUtils.wrapAsAppModel( img, model );
+		LabelImageUtils.importSpotsFromBdvChannel( projectModel, 0, 1 );
+
+		Iterator< Spot > iter = model.getGraph().vertices().iterator();
+		Spot spot = iter.next();
+		double[][] covarianceMatrix = new double[ 3 ][ 3 ];
+		spot.getCovariance( covarianceMatrix );
+		final JamaEigenvalueDecomposition eigenvalueDecomposition = new JamaEigenvalueDecomposition( 3 );
+		eigenvalueDecomposition.decomposeSymmetric( covarianceMatrix );
+		final double[] eigenValues = eigenvalueDecomposition.getRealEigenvalues();
+		double axisA = Math.sqrt( eigenValues[ 0 ] );
+		double axisB = Math.sqrt( eigenValues[ 1 ] );
+		double axisC = Math.sqrt( eigenValues[ 2 ] );
+
+		assertNotNull( spot );
+		assertEquals( 0, spot.getTimepoint() );
+		assertEquals( 2, spot.getDoublePosition( 0 ), 0.01 );
+		assertEquals( 2, spot.getDoublePosition( 1 ), 0.01 );
+		assertEquals( 2, spot.getDoublePosition( 2 ), 0.01 );
+		assertEquals( 0, spot.getInternalPoolIndex() );
+		assertEquals( String.valueOf( pixelValue ), spot.getLabel() );
+		assertEquals( 1, axisA, 0.1d );
+		assertEquals( 1, axisB, 0.1d );
+		assertEquals( 1, axisC, 0.1d );
+		assertEquals( 1, spot.getBoundingSphereRadiusSquared(), 0.2d );
+		assertFalse( iter.hasNext() );
+	}
+
+	@Test
+	public void testImportSpotsFromImgPlus()
+	{
+		LegacyInjector.preinit();
+		double[] center = { 18, 21, 22 };
+		double[][] givenCovariance = {
+				{ 33, 14, 0 },
+				{ 14, 32, 0 },
+				{ 0, 0, 95 }
+		};
+		Spot spot = model.getGraph().addVertex().init( 0, center, givenCovariance );
+		int pixelValue = 1;
+		Img< FloatType > image = createImageFromSpot( spot, pixelValue );
+		ImgPlus< FloatType > imgPlus = createImgPlus( image, new FinalVoxelDimensions( "um", 1, 1, 1 ) );
+		ProjectModel projectModel = DemoUtils.wrapAsAppModel( image, model );
+		LabelImageUtils.importSpotsFromImgPlus( imgPlus, 2.1, projectModel );
+
+		Iterator< Spot > iterator = model.getGraph().vertices().iterator();
+		iterator.next();
+		Spot createdSpot = iterator.next();
+		double[] mean = createdSpot.positionAsDoubleArray();
+		double[][] computedCovariance = new double[ 3 ][ 3 ];
+		createdSpot.getCovariance( computedCovariance );
+
+		logger.debug( "Given center: {}", Arrays.toString( center ) );
+		logger.debug( "Computed mean: {}", Arrays.toString( mean ) );
+		logger.debug( "Given covariance: {}", Arrays.deepToString( givenCovariance ) );
+		logger.debug( "Computed covariance: {}", Arrays.deepToString( computedCovariance ) );
+
+		assertArrayEquals( center, mean, 0.01d );
+		assertArrayEquals( givenCovariance[ 0 ], computedCovariance[ 0 ], 5d );
+		assertArrayEquals( givenCovariance[ 1 ], computedCovariance[ 1 ], 5d );
+		assertArrayEquals( givenCovariance[ 2 ], computedCovariance[ 2 ], 5d );
+		assertEquals( String.valueOf( pixelValue ), createdSpot.getLabel() );
+	}
+
+	@Test
+	public void testDimensionsMatch()
+	{
+		LegacyInjector.preinit();
+		Img< FloatType > image = ArrayImgs.floats( 10, 10, 10, 2 );
+		Model model = new Model();
+		ProjectModel projectModel = DemoUtils.wrapAsAppModel( image, model );
+		ImgPlus< FloatType > imgPlus = createImgPlus( image, new FinalVoxelDimensions( "um", 1, 1, 1 ) );
+		assertTrue( LabelImageUtils.dimensionsMatch( projectModel.getSharedBdvData(), imgPlus ) );
+	}
+
+	@Test
+	public void testGetImgPlusDimensions()
+	{
+		Img< FloatType > image = ArrayImgs.floats( 100, 100, 100, 2 );
+		ImgPlus< FloatType > imgPlus = createImgPlus( image, new FinalVoxelDimensions( "um", 1, 1, 1 ) );
+		long[] dimensions = LabelImageUtils.getImgPlusDimensions( imgPlus );
+		assertArrayEquals( new long[] { 100, 100, 100, 2 }, dimensions );
+	}
+
+	private ImgPlus< FloatType > createImgPlus( final Img< FloatType > img, final VoxelDimensions voxelDimensions )
+	{
+		final CalibratedAxis[] axes = { new DefaultLinearAxis( Axes.X, voxelDimensions.dimension( 0 ) ),
+				new DefaultLinearAxis( Axes.Y, voxelDimensions.dimension( 1 ) ),
+				new DefaultLinearAxis( Axes.Z, voxelDimensions.dimension( 2 ) ), new DefaultLinearAxis( Axes.TIME ) };
+		return new ImgPlus<>( img, "Result", axes );
+	}
+
+	private static Img< FloatType > createImageCubeCorners( int pixelValue )
+	{
+		Img< FloatType > img = new ArrayImgFactory<>( new FloatType() ).create( 4, 4, 4 );
+		RandomAccess< FloatType > ra = img.randomAccess();
+		// 8 corners of a cube
+		ra.setPositionAndGet( 1, 1, 1 ).set( pixelValue );
+		ra.setPositionAndGet( 1, 3, 1 ).set( pixelValue );
+		ra.setPositionAndGet( 3, 1, 1 ).set( pixelValue );
+		ra.setPositionAndGet( 3, 3, 1 ).set( pixelValue );
+		ra.setPositionAndGet( 1, 1, 3 ).set( pixelValue );
+		ra.setPositionAndGet( 1, 3, 3 ).set( pixelValue );
+		ra.setPositionAndGet( 3, 1, 3 ).set( pixelValue );
+		ra.setPositionAndGet( 3, 3, 3 ).set( pixelValue );
+
+		return img;
+	}
+
+	private static AbstractSource< FloatType > createNonLabelImage()
+	{
+		Img< FloatType > img = new ArrayImgFactory<>( new FloatType() ).create( 25, 25, 25 );
+		AtomicInteger value = new AtomicInteger( 0 );
+		LoopBuilder.setImages( img ).forEachPixel( floatType -> floatType.set( value.incrementAndGet() ) );
+
+		return new RandomAccessibleIntervalSource<>( img, new FloatType(), new AffineTransform3D(),
+				"Segmentation" );
+	}
+
+	private static Img< FloatType > createImageFromSpot( final Spot spot, int pixelValue )
+	{
+		long[] dimensions = { 40, 40, 40, 1 };
+		Img< FloatType > image = ArrayImgs.floats( dimensions );
+		AffineTransform3D transform = new AffineTransform3D();
+		AbstractSource< FloatType > frame =
+				new RandomAccessibleIntervalSource<>( Views.hyperSlice( image, 3, 0 ), new FloatType(), transform, "Ellipsoids" );
+
+		final EllipsoidIterable< FloatType > ellipsoidIterable = new EllipsoidIterable<>( frame );
+		ellipsoidIterable.reset( spot );
+		ellipsoidIterable.forEach( pixel -> pixel.set( pixelValue ) );
+		return image;
+	}
+}
+
