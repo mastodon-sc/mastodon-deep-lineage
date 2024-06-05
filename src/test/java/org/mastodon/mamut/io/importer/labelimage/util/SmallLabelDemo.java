bb4a321fce27f319a83e050b8caad4a83486f33c
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-06-05 17:49:24 +0200

    Move generation of Image in SmallLabelDemo

--- src/test/java/org/mastodon/mamut/io/importer/labelimage/util/SmallLabelDemo.java
+++ src/test/java/org/mastodon/mamut/io/importer/labelimage/util/SmallLabelDemo.java
@@ -18,6 +18,9 @@ public class SmallLabelDemo
 		try (final Context context = new Context())
 		{
 			long[] dimensions = { 100, 100, 100 };
+			Img< FloatType > image = ArrayImgs.floats( dimensions );
+			Model model = new Model();
+			ProjectModel projectModel = DemoUtils.wrapAsAppModel( image, model, context );
 
 			int[] center1 = { 20, 80, 50 };
 			double radius1 = 0.5d;
@@ -28,7 +31,6 @@ public class SmallLabelDemo
 			int[] center3 = { 60, 80, 50 };
 			double radius3 = 10d;
 
-			Img< FloatType > image = ArrayImgs.floats( dimensions );
 			SphereRenderer.renderSphere( center1, radius1, 1, image );
 			SphereRenderer.renderSphere( center2, radius2, 2, image );
 			SphereRenderer.renderSphere( center3, radius3, 3, image );
@@ -51,8 +53,6 @@ public class SmallLabelDemo
 
 			LineRenderer.renderLine( start7, end7, 7, image );
 
-			Model model = new Model();
-			ProjectModel projectModel = DemoUtils.wrapAsAppModel( image, model, context );
 			SourceAndConverter< ? > sourceAndConverter = projectModel.getSharedBdvData().getSources().get( 0 );
 			LabelImageUtils.importSpotsFromBdvChannel( projectModel, sourceAndConverter.getSpimSource(), 1d, false );
 			DemoUtils.showBdvWindow( projectModel );
