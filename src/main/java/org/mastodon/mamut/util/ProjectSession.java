7ab7cb3b9d1fdfdafcd7148aba159c4c36e6d6a4
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-06-21 16:15:41 +0200

    Add a new Service MastodonProjectService
    * This service is capable of loading, giving access and closing Mastodon projects, when no longer needed.

---
+++ src/main/java/org/mastodon/mamut/util/ProjectSession.java
@@ -0,0 +1,61 @@
+package org.mastodon.mamut.util;
+
+import org.mastodon.mamut.ProjectModel;
+
+import java.io.File;
+
+/**
+ * A session for a Mastodon project.
+ * <p>
+ * This class represents a session for a Mastodon project. It holds the project model and the file the project was loaded from.
+ * <p>
+ * The session is created by the {@link MastodonProjectService} and should be closed by the client when it is no longer needed.
+ */
+public class ProjectSession implements AutoCloseable
+{
+	private final MastodonProjectService service;
+
+	private final File file;
+
+	private final ProjectModel projectModel;
+
+	/**
+	 * Creates a new project session.
+	 * @param file the file the project was loaded from.
+	 * @param projectModel the project model.
+	 * @param service the service that created this session.
+	 */
+	public ProjectSession( final File file, final ProjectModel projectModel, final MastodonProjectService service )
+	{
+		this.file = file;
+		this.projectModel = projectModel;
+		this.service = service;
+	}
+
+	/**
+	 * Closes this session.
+	 */
+	@Override
+	public void close()
+	{
+		service.releaseSession( this );
+	}
+
+	/**
+	 * Gets the project model.
+	 * @return the project model.
+	 */
+	public ProjectModel getProjectModel()
+	{
+		return projectModel;
+	}
+
+	/**
+	 * Gets the file the project was loaded from.
+	 * @return the file.
+	 */
+	public File getFile()
+	{
+		return file;
+	}
+}
