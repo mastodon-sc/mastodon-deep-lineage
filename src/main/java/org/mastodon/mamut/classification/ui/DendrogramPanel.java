0c44c580c515e13f4cccfefc881fb0151181322f
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-07-25 16:57:37 +0200

    Use different default filenames for dendrogram and classification results

--- src/main/java/org/mastodon/mamut/classification/ui/DendrogramPanel.java
+++ src/main/java/org/mastodon/mamut/classification/ui/DendrogramPanel.java
@@ -340,20 +340,22 @@ public class DendrogramPanel< T > extends JPanel
 		{
 			String exportText = "Export dendrogram as ";
 			JMenuItem pngItem = new JMenuItem( exportText + PNG_EXTENSION.toUpperCase() );
-			pngItem.addActionListener( actionEvent -> chooseFileAndExport( PNG_EXTENSION, DendrogramPanel.this::exportPng ) );
+			pngItem.addActionListener( actionEvent -> chooseFileAndExport( PNG_EXTENSION, "dendrogram", DendrogramPanel.this::exportPng ) );
 			add( pngItem );
 			JMenuItem svgItem = new JMenuItem( exportText + SVG_EXTENSION.toUpperCase() );
-			svgItem.addActionListener( actionEvent -> chooseFileAndExport( SVG_EXTENSION, ( file, resolution ) -> exportSvg( file ) ) );
+			svgItem.addActionListener(
+					actionEvent -> chooseFileAndExport( SVG_EXTENSION, "dendrogram", ( file, resolution ) -> exportSvg( file ) ) );
 			add( svgItem );
 			JMenuItem csvItem = new JMenuItem( "Export classification to CSV" );
 			csvItem.addActionListener(
-					actionEvent -> chooseFileAndExport( CSV_EXTENSION, ( file, resolution ) -> classification.exportCsv( file, tagSet ) ) );
+					actionEvent -> chooseFileAndExport( CSV_EXTENSION, "classification",
+							( file, resolution ) -> classification.exportCsv( file, tagSet ) ) );
 			add( csvItem );
 		}
 
-		private void chooseFileAndExport( final String extension, final ObjIntConsumer< File > exportFunction )
+		private void chooseFileAndExport( final String extension, final String fileName, final ObjIntConsumer< File > exportFunction )
 		{
-			File chosenFile = FileChooser.chooseFile( DendrogramPanel.this, "dendrogram." + extension,
+			File chosenFile = FileChooser.chooseFile( DendrogramPanel.this, fileName + File.separator + extension,
 					new ExtensionFileFilter( extension ), "Save dendrogram to " + extension, FileChooser.DialogType.SAVE );
 			if ( chosenFile != null )
 			{
