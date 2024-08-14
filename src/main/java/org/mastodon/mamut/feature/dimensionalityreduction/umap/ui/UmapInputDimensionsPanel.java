ea0cdc2ee69ec84d231c53b3a5e3825b2167377c
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-08-14 12:33:36 +0200

    Add UmapInputDimensionsPanel, which may serve as a component to display features and let the user select them

---
+++ src/main/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/ui/UmapInputDimensionsPanel.java
@@ -0,0 +1,82 @@
+package org.mastodon.mamut.feature.dimensionalityreduction.umap.ui;
+
+import net.miginfocom.swing.MigLayout;
+import org.mastodon.feature.FeatureModel;
+import org.mastodon.graph.Vertex;
+import org.mastodon.mamut.feature.dimensionalityreduction.umap.util.UmapInputDimension;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import java.util.List;
+import java.util.function.Supplier;
+
+class UmapInputDimensionsPanel< V extends Vertex< ? > > extends JPanel implements Supplier< List< UmapInputDimension< V > > >
+{
+	private final JList< UmapInputDimension< V > > featureList;
+
+	private final DefaultListModel< UmapInputDimension< V > > listModel;
+
+	private final Class< V > vertexType;
+
+	private final FeatureModel featureModel;
+
+	UmapInputDimensionsPanel( final Class< V > vertexType, final FeatureModel featureModel )
+	{
+		super( new MigLayout( "insets 0 0 0 0, fill", "", "" ) );
+		this.vertexType = vertexType;
+		this.featureModel = featureModel;
+		listModel = new DefaultListModel<>();
+		featureList = new JList<>( listModel );
+		updateItemList();
+		selectAllDimensions();
+		initBehavior();
+		initLayout();
+	}
+
+	private void initLayout()
+	{
+		JScrollPane listScrollPane = new JScrollPane( featureList );
+		add( new JLabel( "Features:" ) );
+		add( listScrollPane, "span, growx, pushx, growy, pushy, wrap" );
+	}
+
+	private void initBehavior()
+	{
+		featureList.setSelectionMode( ListSelectionModel.MULTIPLE_INTERVAL_SELECTION );
+		featureList.getSelectionModel().clearSelection();
+		featureList.getSelectionModel().setSelectionInterval( 0, listModel.getSize() - 1 );
+		final FeatureModel.FeatureModelListener featureModelListener = this::updateItemList;
+		featureModel.listeners().add( featureModelListener );
+	}
+
+	private void selectAllDimensions()
+	{
+		ListSelectionModel selectionModel = featureList.getSelectionModel();
+		selectionModel.clearSelection();
+		selectionModel.setSelectionInterval( 0, listModel.getSize() - 1 );
+	}
+
+	private void updateItemList()
+	{
+		listModel.clear();
+		List< UmapInputDimension< V > > items = UmapInputDimension.getListFromFeatureModel( featureModel, vertexType );
+		for ( UmapInputDimension< V > item : items )
+			listModel.addElement( item );
+	}
+
+	int getNumberOfFeatures()
+	{
+		featureList.getSelectedValuesList();
+		return listModel.getSize();
+	}
+
+	@Override
+	public List< UmapInputDimension< V > > get()
+	{
+		return featureList.getSelectedValuesList();
+	}
+}
