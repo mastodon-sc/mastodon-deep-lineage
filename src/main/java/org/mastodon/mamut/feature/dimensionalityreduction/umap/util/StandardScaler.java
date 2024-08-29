1217099c9fe75aa3f8a0c410ecd0835fd9a115bc
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-08-29 16:42:12 +0200

    Replace custom Standardization implementation by the one from apache commons math3 library

--- src/main/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/util/StandardScaler.java
+++ src/main/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/util/StandardScaler.java
@@ -1,8 +1,6 @@
 package org.mastodon.mamut.feature.dimensionalityreduction.umap.util;
 
-import org.apache.commons.math3.stat.descriptive.moment.Variance;
-
-import java.util.Arrays;
+import org.apache.commons.math3.stat.StatUtils;
 
 /**
  * Standardize vectors by removing the mean and scaling to unit variance.
@@ -16,44 +14,6 @@ public class StandardScaler
 	private StandardScaler()
 	{
 		// prevent instantiation
-	}
-
-	/**
-	 * Standardizes a vector by removing the mean and scaling to unit variance.
-	 * <br>
-	 * This method calculates the mean and variance of the input vector,
-	 * then scales each element of the vector by subtracting the mean and dividing by the standard deviation.
-	 *
-	 * @param vector The input vector to be standardized.
-	 * @return A new vector where each element is scaled such that the mean of the vector equals to zero and variance of the values is unit variance.
-	 */
-	public static double[] standardizeVector( double[] vector )
-	{
-		double mean = Arrays.stream( vector ).sum() / vector.length;
-		Variance varianceCalculator = new Variance();
-		double variance = varianceCalculator.evaluate( vector );
-		return scaleVector( vector, mean, variance );
-	}
-
-	/**
-	 * Scales a vector by removing the mean and dividing by the standard deviation.
-	 * <br>
-	 * This method takes an input vector and scales each element by subtracting the mean and dividing by the standard deviation.
-	 *
-	 * @param vector The input vector to be scaled.
-	 * @param mean The mean of the input vector.
-	 * @param variance The variance of the input vector.
-	 * @return A new vector where each element is scaled such that the mean of the vector equals to zero and variance of the values is unit variance.
-	 */
-	public static double[] scaleVector( double[] vector, double mean, double variance )
-	{
-		double standardDeviation = Math.sqrt( variance );
-		double[] scaledVector = new double[ vector.length ];
-		for ( int i = 0; i < vector.length; i++ )
-		{
-			scaledVector[ i ] = standardDeviation == 0 ? 0 : ( vector[ i ] - mean ) / standardDeviation;
-		}
-		return scaledVector;
 	}
 
 	/**
@@ -74,12 +34,11 @@ public class StandardScaler
 		{
 			column[ i ] = matrix[ i ][ columnIndex ];
 		}
-
-		double[] standardizedColumn = standardizeVector( column );
+		double[] standardizedColumn = StatUtils.normalize( column );
 
 		for ( int i = 0; i < numRows; i++ )
 		{
-			matrix[ i ][ columnIndex ] = standardizedColumn[ i ];
+			matrix[ i ][ columnIndex ] = Double.isNaN( standardizedColumn[ i ] ) ? 0d : standardizedColumn[ i ]; // replace NaNs with 0
 		}
 	}
 
--- src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/util/StandardScalerTest.java
+++ src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/util/StandardScalerTest.java
@@ -7,59 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 class StandardScalerTest
 {
 	@Test
-	void scaleVector_withZeroVariance()
-	{
-		double[] vector = new double[] { 3, 3, 3, 3, 3 };
-		double[] scaledVector = StandardScaler.standardizeVector( vector );
-		assertArrayEquals( new double[] { 0, 0, 0, 0, 0 }, scaledVector );
-	}
-
-	@Test
-	void standardizeVector()
-	{
-		double[] vector = new double[] { 1, 2, 3, 4, 5 };
-		double[] scaledVector = StandardScaler.standardizeVector( vector );
-		assertArrayEquals( new double[] { -1.2649110640673518, -0.6324555320336759, 0, 0.6324555320336759, 1.2649110640673518 },
-				scaledVector );
-	}
-
-	@Test
-	void standardizeVector_withNegativeValues()
-	{
-		double[] vector = new double[] { -1, -2, -3, -4, -5 };
-		double[] scaledVector = StandardScaler.standardizeVector( vector );
-		assertArrayEquals( new double[] { 1.2649110640673518, 0.6324555320336759, 0, -0.6324555320336759, -1.2649110640673518 },
-				scaledVector );
-	}
-
-	@Test
-	void standardizeVector_withSingleValue()
-	{
-		double[] vector = new double[] { 42 };
-		double[] scaledVector = StandardScaler.standardizeVector( vector );
-		assertArrayEquals( new double[] { 0 }, scaledVector );
-	}
-
-	@Test
-	void standardizeVector_withEmptyVector()
-	{
-		double[] vector = new double[] {};
-		double[] scaledVector = StandardScaler.standardizeVector( vector );
-		assertArrayEquals( new double[] {}, scaledVector );
-	}
-
-	@Test
-	void testScaleVector()
-	{
-		double[] vector = new double[] { 1, 2, 3, 4, 5 };
-		double mean = 3;
-		double variance = 2;
-		double[] scaledVector = StandardScaler.scaleVector( vector, mean, variance );
-		assertArrayEquals( scaledVector,
-				new double[] { -1.414213562373095, -0.7071067811865475, 0, 0.7071067811865475, 1.414213562373095 } );
-	}
-
-	@Test
 	void standardizeColumns_withMultipleColumns()
 	{
 		double[][] array = {
