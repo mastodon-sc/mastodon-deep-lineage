0fea5fca5c442ed27f745e9beb5cb99835100daf
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-09-02 12:01:55 +0200

    Extend StandardScalerTest to contain an example with NaN values

--- src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/util/StandardScalerTest.java
+++ src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/util/StandardScalerTest.java
@@ -86,4 +86,25 @@ class StandardScalerTest
 		StandardScaler.standardizeColumns( array );
 		assertArrayEquals( expected, array );
 	}
+
+	@Test
+	void standardizeColumns_withNaNValue()
+	{
+		double[][] array = {
+				{ 1, 2, 1 },
+				{ 2, 2, 2 },
+				{ 3, Double.NaN, 3 },
+				{ 4, 2, 4 },
+				{ 5, 2, 5 }
+		};
+		double[][] expected = {
+				{ -1.2649110640673518, 0, -1.2649110640673518 },
+				{ -0.6324555320336759, 0, -0.6324555320336759 },
+				{ 0, 0, 0 },
+				{ 0.6324555320336759, 0, 0.6324555320336759 },
+				{ 1.2649110640673518, 0, 1.2649110640673518 }
+		};
+		StandardScaler.standardizeColumns( array );
+		assertArrayEquals( expected, array );
+	}
 }
