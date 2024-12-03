cf05bd983ce8274d120cefacd733dd5b00d6072b
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-12-03 16:29:30 +0100

    Replace t-SNE library and enable t-SNE unit tests
    * The previously used library com.github.lejon.T-SNE-Java has classpath conflicts with ejml-0.25.jar which comes with Fiji

--- src/test/java/org/mastodon/mamut/feature/branch/dimensionalityreduction/tsne/BranchTSneFeatureTest.java
+++ src/test/java/org/mastodon/mamut/feature/branch/dimensionalityreduction/tsne/BranchTSneFeatureTest.java
@@ -42,7 +42,6 @@ import java.util.function.Supplier;
 import net.imglib2.util.Cast;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mastodon.feature.Dimension;
 import org.mastodon.feature.FeatureModel;
