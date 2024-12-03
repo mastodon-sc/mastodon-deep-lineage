b58be9e027e8b5a7b562608e217f5e4b29b24065
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-12-03 16:29:30 +0100

    Replace t-SNE library and enable t-SNE unit tests
    * The previously used library com.github.lejon.T-SNE-Java has classpath conflicts with ejml-0.25.jar which comes with Fiji

--- src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/tsne/TSneDemoTgmmMini.java
+++ src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/tsne/TSneDemoTgmmMini.java
@@ -7,15 +7,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.jujutsu.tsne.TSneConfiguration;
-import com.jujutsu.tsne.barneshut.BarnesHutTSne;
-import com.jujutsu.tsne.barneshut.ParallelBHTsne;
-import com.jujutsu.utils.TSneUtils;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
 
 import org.mastodon.mamut.feature.dimensionalityreduction.PlotPoints;
 import org.mastodon.mamut.feature.dimensionalityreduction.util.StandardScaler;
+
+import smile.manifold.TSNE;
 
 public class TSneDemoTgmmMini
 {
@@ -34,21 +32,15 @@ public class TSneDemoTgmmMini
 		}
 		double[][] inputData = data.toArray( new double[ data.size() ][ data.get( 0 ).length ] );
 		StandardScaler.standardizeColumns( inputData );
-		TSneConfiguration config = setUpTSne( inputData );
-		BarnesHutTSne tsne = new ParallelBHTsne(); // according to https://github.com/lejon/T-SNE-Java/ the parallel version is faster at same accuracy
-		double[][] result = tsne.tsne( config );
-		result = Arrays.stream( result ).map( row -> Arrays.stream( row ).map( value -> value * 5d ).toArray() ) // scale up
+		double[][] result = setUpTSne( inputData );
+		result = Arrays.stream( result ).map( row -> Arrays.stream( row ).map( value -> value * 2d ).toArray() ) // scale up
 				.toArray( double[][]::new );
 		PlotPoints.plot( null, result, null );
 	}
 
-	static TSneConfiguration setUpTSne( double[][] inputData )
+	static double[][] setUpTSne( double[][] inputData )
 	{
-		// Recommendations for t-SNE defaults: https://scikit-learn.org/stable/modules/generated/sklearn.manifold.TSNE.html
-		int initialDimensions = 50; // used if PCA is true and dimensions of the input data are greater than this value
-		double perplexity = 30d; // recommended value is between 5 and 50
-		int maxIterations = 1000; // should be at least 250
-
-		return TSneUtils.buildConfig( inputData, 2, initialDimensions, perplexity, maxIterations, true, 0.5d, false, true );
+		TSNE tsne = new TSNE( inputData, 2, 30d, 200, 1000 );
+		return tsne.coordinates;
 	}
 }
