f5b1abf203802495e43b871398d3ccec8bdcda8c
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2024-12-03 16:29:30 +0100

    Replace t-SNE library and enable t-SNE unit tests
    * The previously used library com.github.lejon.T-SNE-Java has classpath conflicts with ejml-0.25.jar which comes with Fiji

--- src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/tsne/TSneDemoTgmmMini.java
+++ src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/tsne/TSneDemoTgmmMini.java
@@ -7,48 +7,46 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.jujutsu.tsne.TSneConfiguration;
-import com.jujutsu.tsne.barneshut.BarnesHutTSne;
-import com.jujutsu.tsne.barneshut.ParallelBHTsne;
-import com.jujutsu.utils.TSneUtils;
+import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvValidationException;
 
 import org.mastodon.mamut.feature.dimensionalityreduction.PlotPoints;
 import org.mastodon.mamut.feature.dimensionalityreduction.util.StandardScaler;
+
+import smile.manifold.TSNE;
 
 public class TSneDemoTgmmMini
 {
 	public static void main( String[] args ) throws IOException, CsvValidationException
 	{
 		File file = new File( "src/test/resources/org/mastodon/mamut/feature/dimensionalityreduction/tgmm-mini-spot.csv" );
-		CSVReader reader = new CSVReader( new FileReader( file ) );
-		List< double[] > data = new ArrayList<>();
-		reader.readNext(); // skip header
-		for ( String[] nextLine; ( nextLine = reader.readNext() ) != null; )
+		CSVParserBuilder parserBuilder = new CSVParserBuilder().withSeparator( ',' );
+		CSVReaderBuilder readerBuilder = new CSVReaderBuilder( new FileReader( file ) ).withCSVParser( parserBuilder.build() );
+		try (CSVReader reader = readerBuilder.build())
 		{
-			double[] values = new double[ nextLine.length ];
-			for ( int i = 0; i < nextLine.length; i++ )
-				values[ i ] = Double.parseDouble( nextLine[ i ] );
-			data.add( values );
+			List< double[] > data = new ArrayList<>();
+			reader.readNext(); // skip header
+			for ( String[] nextLine; ( nextLine = reader.readNext() ) != null; )
+			{
+				double[] values = new double[ nextLine.length ];
+				for ( int i = 0; i < nextLine.length; i++ )
+					values[ i ] = Double.parseDouble( nextLine[ i ] );
+				data.add( values );
+			}
+			double[][] inputData = data.toArray( new double[ data.size() ][ data.get( 0 ).length ] );
+			StandardScaler.standardizeColumns( inputData );
+			double[][] result = setUpTSne( inputData );
+			result = Arrays.stream( result ).map( row -> Arrays.stream( row ).map( value -> value * 2d ).toArray() ) // scale up
+					.toArray( double[][]::new );
+			PlotPoints.plot( null, result, null );
 		}
-		double[][] inputData = data.toArray( new double[ data.size() ][ data.get( 0 ).length ] );
-		StandardScaler.standardizeColumns( inputData );
-		TSneConfiguration config = setUpTSne( inputData );
-		BarnesHutTSne tsne = new ParallelBHTsne(); // according to https://github.com/lejon/T-SNE-Java/ the parallel version is faster at same accuracy
-		double[][] result = tsne.tsne( config );
-		result = Arrays.stream( result ).map( row -> Arrays.stream( row ).map( value -> value * 5d ).toArray() ) // scale up
-				.toArray( double[][]::new );
-		PlotPoints.plot( null, result, null );
 	}
 
-	static TSneConfiguration setUpTSne( double[][] inputData )
+	static double[][] setUpTSne( double[][] inputData )
 	{
-		// Recommendations for t-SNE defaults: https://scikit-learn.org/stable/modules/generated/sklearn.manifold.TSNE.html
-		int initialDimensions = 50; // used if PCA is true and dimensions of the input data are greater than this value
-		double perplexity = 30d; // recommended value is between 5 and 50
-		int maxIterations = 1000; // should be at least 250
-
-		return TSneUtils.buildConfig( inputData, 2, initialDimensions, perplexity, maxIterations, true, 0.5d, false, true );
+		TSNE tsne = new TSNE( inputData, 2, 30d, 200, 1000 );
+		return tsne.coordinates;
 	}
 }
