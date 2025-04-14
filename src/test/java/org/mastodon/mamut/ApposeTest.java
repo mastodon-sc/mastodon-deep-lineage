5de5831d8f8084b41361e3ebd377bb74892bba14
stha735e <stefan.hahmann@tu-dresden.de>
2025-04-14 17:27:01 +0200

    Modify Appose test to (in linux) working 3D example

--- src/test/java/org/mastodon/mamut/ApposeTest.java
+++ src/test/java/org/mastodon/mamut/ApposeTest.java
@@ -5,58 +5,65 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.sun.jna.Pointer;
-import com.sun.jna.platform.win32.Kernel32;
-import com.sun.jna.platform.win32.WinBase;
-import com.sun.jna.platform.win32.WinNT;
-
-import net.imglib2.appose.NDArrays;
-import net.imglib2.appose.ShmImg;
-import net.imglib2.img.Img;
-import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.type.numeric.real.FloatType;
-
 import org.apposed.appose.Appose;
 import org.apposed.appose.Environment;
 import org.apposed.appose.NDArray;
 import org.apposed.appose.Service;
 
+import bdv.util.Bdv;
+import bdv.util.BdvFunctions;
+import bdv.util.BdvStackSource;
 import io.scif.img.ImgOpener;
+import net.imglib2.appose.NDArrays;
+import net.imglib2.appose.ShmImg;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.real.FloatType;
 
 public class ApposeTest
 {
-	public static < T extends net.imglib2.type.NativeType< T > > void main( String[] args ) throws IOException, InterruptedException
+	public static void main( String[] args ) throws IOException, InterruptedException
 	{
 		// Specify the path to the TIFF file
-		String filePath = "target/test-classes/org/mastodon/mamut/nuclei_3d.tif";
+		// String filePath = "target/test-classes/org/mastodon/mamut/nuclei_3d.tif";
+		String filePath = "/home/pol_bia/stha735e/Documents/Mastodon/1135_n_stain_TO-PRO-3.tif";
+		String starDistModelPath = "/home/pol_bia/stha735e/StarDist";
 		ImgOpener imgOpener = new ImgOpener();
-		// Img< FloatType > img = imgOpener.openImgs( filePath, new FloatType() ).get( 0 );
-		Img< FloatType > img = ArrayImgs.floats( 20, 20, 20 );
+		Img< FloatType > img = imgOpener.openImgs( filePath, new FloatType() ).get( 0 );
+		//Img< FloatType > img = ArrayImgs.floats( 20, 20, 20 ); // empty image
 		Img< FloatType > shmImg = ShmImg.copyOf( img );
 
 		StringBuilder sb = new StringBuilder();
-		// sb.append( "import skimage" ).append( "\n" );
-		// sb.append( "narr = image.ndarray()" ).append( "\n" );
-		// sb.append( "import numpy as np" ).append( "\n" );
-		// sb.append( "from csbdeep.utils import normalize" ).append( "\n" );
-		// sb.append( "from stardist.models import StarDist3D" ).append( "\n\n" );
+		sb.append( "import numpy as np" ).append( "\n" );
+		sb.append( "import appose").append( "\n" );
+		sb.append( "from csbdeep.utils import normalize" ).append( "\n" );
+		sb.append( "from stardist.models import StarDist3D" ).append( "\n" );
 
-		// sb.append( "np.random.seed(42)" ).append( "\n" );
-		//sb.append( "axes_normalize = (0, 1, 2)" ).append( "\n" );
-		//sb.append( "model = StarDist3D.from_pretrained('3D_demo')" ).append( "\n" );
-		//sb.append( "narr = image.ndarray()" ).append( "\n" );
-		//sb.append( "image_normalized = normalize(narr, 1, 99.8, axis=axes_normalize)" ).append( "\n" );
-		//sb.append( "label_image, details = model.predict_instances(image_normalized)" ).append( "\n" );
-		//sb.append( "shared = appose.NDArray(image.dtype, image.shape)" ).append( "\n" );
-		//sb.append( "shared.ndarray()[:] = label_image" ).append( "\n" );
-		//sb.append( "task.outputs['label_image'] = shared" ).append( "\n" );
+		sb.append( "np.random.seed(6)" ).append( "\n" );
+		sb.append( "axes_normalize = (0, 1, 2)" ).append( "\n" );
+		sb.append( "print(\"Loading StarDist pretrained 3D model\")").append("\n");
+		// sb.append( "model = StarDist3D.from_pretrained('3D_demo')" ).append( "\n" );
+		sb.append( "model = StarDist3D(None, name='stardist-plant-nuclei-3d', basedir=r\""+starDistModelPath+"\")" ).append( "\n" );
+		sb.append( "narr = image.ndarray()" ).append( "\n" );
+		sb.append( "image_normalized = normalize(narr, 1, 99.8, axis=axes_normalize)" ).append( "\n" );
+		sb.append( "print(\"Image shape:\", image_normalized.shape)").append("\n");
+		sb.append( "guessed_tiles = model._guess_n_tiles(image_normalized)").append("\n");
+		sb.append( "print(\"Guessed tiling:\", guessed_tiles)").append("\n");
+		sb.append( "label_image, details = model.predict_instances(image_normalized, axes='ZYX', n_tiles=guessed_tiles)" ).append( "\n" );
+		sb.append( "shared = appose.NDArray(image.dtype, image.shape)" ).append( "\n" );
+		sb.append( "shared.ndarray()[:] = label_image" ).append( "\n" );
+		sb.append( "task.outputs['label_image'] = shared" ).append( "\n" );
 		String script = sb.toString();
 
-		String envYmlPath = "target/test-classes/org/mastodon/mamut/skimage.yml";
+		System.out.println( "Script: " + script );
+
+		String envYmlPath = "target/test-classes/org/mastodon/mamut/stardist.yml";
 		File envYmlFile = new File( envYmlPath );
-		System.out.println( envYmlFile.getAbsoluteFile() );
+		System.out.println( "env yml file: " + envYmlFile.getAbsoluteFile() );
 		Environment env = Appose.file( envYmlFile, "environment.yml" ).logDebug().build();
 		System.out.println( "Created environment" );
+
+		// Display the first image in a new BDV instance
+		BdvStackSource<?> bdvSource1 = BdvFunctions.show(img, "Original Image");
 
 		try (Service python = env.python())
 		{
@@ -80,9 +87,12 @@ public class ApposeTest
 			// Access the `label_image` output NDArray.
 			NDArray labelImageArray = ( NDArray ) task.outputs.get( "label_image" );
 			// Wrap the NDArray to an Img.
-			// Img< FloatType > labelImage = new ShmImg<>( labelImageArray );
+			Img< FloatType > labelImage = new ShmImg<>( labelImageArray );
 
-			// System.out.println( "label image size: " + labelImage.size() );
+        	// Add the second image as a second channel in the same BDV instance
+        	BdvFunctions.show(labelImage, "Segmentation", Bdv.options().addTo(bdvSource1.getBdvHandle()));
+
+			System.out.println( "label image size: " + labelImage.size() );
 		}
 	}
 }
