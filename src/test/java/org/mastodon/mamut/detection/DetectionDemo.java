35f482e510441777132839ed8c3e776c79316c41
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2025-04-17 15:18:03 +0200

    Do not catch exceptions in DetectionDemo anymore

--- src/test/java/org/mastodon/mamut/detection/DetectionDemo.java
+++ src/test/java/org/mastodon/mamut/detection/DetectionDemo.java
@@ -1,7 +1,5 @@
 package org.mastodon.mamut.detection;
 
-import java.io.IOException;
-
 import net.imglib2.img.Img;
 import net.imglib2.type.numeric.real.FloatType;
 
@@ -20,33 +18,26 @@ public class DetectionDemo
 		String starDistModelPath = "/home/pol_bia/stha735e/StarDist";
 		ImgOpener imgOpener = new ImgOpener();
 		Img< FloatType > img = imgOpener.openImgs( filePath, new FloatType() ).get( 0 );
-		try
-		{
-			long startTime = System.currentTimeMillis();
-			Cellpose cellpose = new Cellpose( Cellpose.MODEL_TYPE.CYTO );
-			Img< FloatType > cellPoseSegmentation = cellpose.segmentImage( img );
-			long endTime = System.currentTimeMillis();
-			System.out.println( "StarDist segmentation time: " + ( endTime - startTime ) + " ms" );
-			startTime = System.currentTimeMillis();
-			StarDist3D starDist3D = new StarDist3D( starDistModelPath );
-			Img< FloatType > starDistSegmentation = starDist3D.segmentImage( img );
-			endTime = System.currentTimeMillis();
-			System.out.println( "Cellpose segmentation time: " + ( endTime - startTime ) + " ms" );
+		long startTime = System.currentTimeMillis();
+		Cellpose cellpose = new Cellpose( Cellpose.MODEL_TYPE.CYTO );
+		Img< FloatType > cellPoseSegmentation = cellpose.segmentImage( img );
+		long endTime = System.currentTimeMillis();
+		System.out.println( "StarDist segmentation time: " + ( endTime - startTime ) + " ms" );
+		startTime = System.currentTimeMillis();
+		StarDist3D starDist3D = new StarDist3D( starDistModelPath );
+		Img< FloatType > starDistSegmentation = starDist3D.segmentImage( img );
+		endTime = System.currentTimeMillis();
+		System.out.println( "Cellpose segmentation time: " + ( endTime - startTime ) + " ms" );
 
-			// Display the first image in a new BDV instance
-			BdvStackSource< ? > bdvSource1 = BdvFunctions.show( img, "Original Image" );
+		// Display the first image in a new BDV instance
+		BdvStackSource< ? > bdvSource1 = BdvFunctions.show( img, "Original Image" );
 
-			// Add cellpose segmentation as a second channel in the same BDV instance
-			if ( cellPoseSegmentation != null )
-				BdvFunctions.show( cellPoseSegmentation, "Cellpose Segmentation", Bdv.options().addTo( bdvSource1.getBdvHandle() ) );
+		// Add cellpose segmentation as a second channel in the same BDV instance
+		if ( cellPoseSegmentation != null )
+			BdvFunctions.show( cellPoseSegmentation, "Cellpose Segmentation", Bdv.options().addTo( bdvSource1.getBdvHandle() ) );
 
-			// Add star dist segmentation as a third channel in the same BDV instance
-			if ( starDistSegmentation != null )
-				BdvFunctions.show( starDistSegmentation, "StarDist Segmentation", Bdv.options().addTo( bdvSource1.getBdvHandle() ) );
-		}
-		catch ( InterruptedException | IOException e )
-		{
-			System.err.println( e.getMessage() );
-		}
+		// Add star dist segmentation as a third channel in the same BDV instance
+		if ( starDistSegmentation != null )
+			BdvFunctions.show( starDistSegmentation, "StarDist Segmentation", Bdv.options().addTo( bdvSource1.getBdvHandle() ) );
 	}
 }
