fa2c9cb01eb33b61b0fe43893d424de9a8867880
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2025-05-27 18:01:21 +0200

    Make settings available to detector in Cellpose3DetectorTest

--- src/test/java/org/mastodon/mamut/detection/cellpose/Cellpose3DetectorTest.java
+++ src/test/java/org/mastodon/mamut/detection/cellpose/Cellpose3DetectorTest.java
@@ -1,6 +1,9 @@
 package org.mastodon.mamut.detection.cellpose;
 
+import java.lang.reflect.Field;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
@@ -8,6 +11,7 @@ import net.imglib2.type.numeric.real.FloatType;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ReflectionUtils;
 import org.mastodon.mamut.ProjectModel;
 import org.mastodon.mamut.io.importer.labelimage.util.DemoUtils;
 import org.mastodon.mamut.io.importer.labelimage.util.SphereRenderer;
@@ -19,7 +23,7 @@ class Cellpose3DetectorTest
 {
 
 	@Test
-	void testCompute()
+	void testCompute() throws IllegalAccessException
 	{
 		Cellpose3Detector detector = new Cellpose3Detector();
 		Model model = new Model();
@@ -28,11 +32,19 @@ class Cellpose3DetectorTest
 		{
 			Img< FloatType > img = ArrayImgs.floats( 12, 12, 12 );
 			SphereRenderer.renderSphere( new int[] { 5, 5, 5 }, 5, 100, img );
-			context.inject( detector );
+			context.inject( detector ); // make sure the detector is initialized with the context
+
+			// make settings available for the detector
+			Field settingsField = ReflectionUtils.findFields( Cellpose3Detector.class, f -> f.getName().equals( "settings" ),
+					ReflectionUtils.HierarchyTraversalMode.TOP_DOWN ).get( 0 );
+			settingsField.setAccessible( true );
+			Map< String, Object > settings = new HashMap<>();
+			settings.put( DetectorKeys.KEY_MIN_TIMEPOINT, 0 );
+			settings.put( DetectorKeys.KEY_MAX_TIMEPOINT, 0 );
+			settings.put( DetectorKeys.KEY_SETUP_ID, 0 );
+			settingsField.set( detector, settings );
+
 			ProjectModel projectModel = DemoUtils.wrapAsAppModel( img, model, context );
-			detector.getDefaultSettings().put( DetectorKeys.KEY_MIN_TIMEPOINT, 0 );
-			detector.getDefaultSettings().put( DetectorKeys.KEY_MAX_TIMEPOINT, 0 );
-			detector.getDefaultSettings().put( DetectorKeys.KEY_SETUP_ID, 0 );
 			Assertions.assertEquals( 0, model.getGraph().vertices().size() ); // before detection
 			detector.compute( Collections.singletonList( projectModel.getSharedBdvData().getSources().get( 0 ) ), model.getGraph() );
 			Assertions.assertEquals( 1, model.getGraph().vertices().size() ); // after detection
