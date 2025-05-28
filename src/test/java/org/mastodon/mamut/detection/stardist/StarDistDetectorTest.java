fab1ff0b69b00dfd3866bfe9275db5ce4ceb8107
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2025-05-28 11:33:09 +0200

    Change StarDistDetectorTest to use example image nuclei_3d.tif

--- src/test/java/org/mastodon/mamut/detection/stardist/StarDistDetectorTest.java
+++ src/test/java/org/mastodon/mamut/detection/stardist/StarDistDetectorTest.java
@@ -1,12 +1,14 @@
 package org.mastodon.mamut.detection.stardist;
 
+import java.io.File;
 import java.lang.reflect.Field;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import net.imglib2.img.Img;
-import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.type.numeric.real.FloatType;
 
 import org.junit.jupiter.api.Assertions;
@@ -14,24 +16,30 @@ import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.ReflectionUtils;
 import org.mastodon.mamut.ProjectModel;
 import org.mastodon.mamut.io.importer.labelimage.util.DemoUtils;
-import org.mastodon.mamut.io.importer.labelimage.util.SphereRenderer;
 import org.mastodon.mamut.model.Model;
 import org.mastodon.tracking.detection.DetectorKeys;
 import org.mastodon.tracking.mamut.trackmate.wizard.descriptors.StarDistDetectorDescriptor;
 import org.scijava.Context;
 
+import io.scif.img.ImgOpener;
+
 class StarDistDetectorTest
 {
 	@Test
-	void testCompute3D() throws IllegalAccessException
+	void testCompute3D() throws IllegalAccessException, URISyntaxException
 	{
 		StarDistDetector detector = new StarDistDetector();
 		Model model = new Model();
 
 		try (Context context = new Context())
 		{
-			Img< FloatType > img = ArrayImgs.floats( 12, 12, 12 );
-			SphereRenderer.renderSphere( new int[] { 5, 5, 5 }, 5, 100, img );
+			ImgOpener imgOpener = new ImgOpener();
+			URL url = getClass().getClassLoader().getResource( "org/mastodon/mamut/appose/nuclei_3d.tif" );
+			Assertions.assertNotNull( url, "Resource not found" );
+
+			File file = new File( url.toURI() );
+			Assertions.assertTrue( file.exists(), "File does not exist" );
+			Img< FloatType > img = imgOpener.openImgs( file.getAbsolutePath(), new FloatType() ).get( 0 );
 			context.inject( detector ); // make sure the detector is initialized with the context
 
 			// set up the detector settings
@@ -50,7 +58,7 @@ class StarDistDetectorTest
 			ProjectModel projectModel = DemoUtils.wrapAsAppModel( img, model, context );
 			Assertions.assertEquals( 0, model.getGraph().vertices().size() ); // before detection
 			detector.compute( Collections.singletonList( projectModel.getSharedBdvData().getSources().get( 0 ) ), model.getGraph() );
-			Assertions.assertEquals( 1, model.getGraph().vertices().size() ); // after detection
+			Assertions.assertNotEquals( 0, model.getGraph().vertices().size() ); // after detection
 		}
 	}
 }
