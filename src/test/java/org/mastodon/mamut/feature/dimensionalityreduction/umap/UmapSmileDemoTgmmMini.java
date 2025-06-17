11671453fa65e47cbe08d71b4f58b85325022839
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2025-06-17 17:20:36 +0200

    Reactivate smile demo applications

--- src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/UmapSmileDemoTgmmMini.java
+++ src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/UmapSmileDemoTgmmMini.java
@@ -26,7 +26,6 @@ public class UmapSmileDemoTgmmMini
 
 	private static final Logger logger = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() );
 
-	/*
 	public static void main( String[] args ) throws IOException, CsvValidationException
 	{
 		File file = new File( "src/test/resources/org/mastodon/mamut/feature/dimensionalityreduction/tgmm-mini-spot.csv" );
@@ -62,6 +61,4 @@ public class UmapSmileDemoTgmmMini
 		int nNeighbors = 15;
 		return UMAP.of( data, MathEx::distance, nNeighbors, 2, iterations, 1, minDist, 1.0, 5, 1 );
 	}
-	
-	 */
 }
