b9ce720a270ebc2af14f7257e209b3e60ac53270
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2025-06-20 15:27:25 +0200

    Refactor UMAP test to use `Properties` for configuration and update parameter setup

--- src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/UmapSmileTest.java
+++ src/test/java/org/mastodon/mamut/feature/dimensionalityreduction/umap/UmapSmileTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
+import java.util.Properties;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mastodon.mamut.feature.dimensionalityreduction.DimensionalityReductionTestUtils;
 import org.mastodon.mamut.feature.dimensionalityreduction.RandomDataTools;
@@ -13,13 +13,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import smile.manifold.UMAP;
-import smile.math.MathEx;
 
 class UmapSmileTest
 {
 	private static final Logger logger = LoggerFactory.getLogger( MethodHandles.lookup().lookupClass() );
 
-	/*
 	@Test
 	void test()
 	{
@@ -29,11 +27,24 @@ class UmapSmileTest
 		double[][] sampleData = RandomDataTools.generateSampleData( numCluster1, numCluster2 );
 	
 		// Recommendations for UMAP defaults: https://github.com/lmcinnes/umap/blob/a012b9d8751d98b94935ca21f278a54b3c3e1b7f/umap/umap_.py#L1073
-		int iterations = sampleData.length < 10_000 ? 500 : 200;
 		double minDist = 0.1;
 		int nNeighbors = 15;
+		int d = 2;
+		double learningRate = 1.0;
+		double spread = 1.0;
+		int negativeSampleRate = 5;
+		double repulsionStrength = 1.0;
 		long t0 = System.currentTimeMillis();
-		double[][] umapResult = UMAP.of( sampleData, MathEx::distance, nNeighbors, 2, iterations, 1, minDist, 1.0, 5, 1 );
+		Properties props = new Properties();
+		props.setProperty( "smile.umap.k", String.valueOf( nNeighbors ) );
+		props.setProperty( "smile.umap.d", String.valueOf( d ) );
+		props.setProperty( "smile.umap.learning_rate", String.valueOf( learningRate ) );
+		props.setProperty( "smile.umap.min_dist", String.valueOf( minDist ) );
+		props.setProperty( "smile.umap.spread", String.valueOf( spread ) );
+		props.setProperty( "smile.umap.negative_samples", String.valueOf( negativeSampleRate ) );
+		props.setProperty( "smile.umap.repulsion_strength", String.valueOf( repulsionStrength ) );
+
+		double[][] umapResult = UMAP.fit( sampleData, UMAP.Options.of( props ) );
 		logger.info( "UMAP took {} ms", System.currentTimeMillis() - t0 );
 	
 		assertEquals( 2, umapResult[ 0 ].length );
@@ -44,6 +55,4 @@ class UmapSmileTest
 	
 		DimensionalityReductionTestUtils.testNonOverlappingClusters( umapResult1, umapResult2 ); // should pass
 	}
-	
-	 */
 }
