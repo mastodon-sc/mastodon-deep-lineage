e9fad044ce65726aca3f5ec124902ba3ece44569
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2025-06-26 13:57:58 +0200

    Add new class SelectedRootsFinder
    * This class finds root spots in a set of selected spots
    * Spots are considered being root either if they have no predecessor at all (i.e. no incoming edge) OR(!) if they have no selected predecessor

---
+++ src/main/java/org/mastodon/mamut/lineagemodules/util/SelectedRootsFinder.java
@@ -0,0 +1,65 @@
+package org.mastodon.mamut.lineagemodules.util;
+
+import org.mastodon.collection.RefSet;
+import org.mastodon.graph.Edge;
+import org.mastodon.graph.ReadOnlyGraph;
+import org.mastodon.graph.Vertex;
+import org.mastodon.graph.algorithm.AbstractGraphAlgorithm;
+import org.mastodon.model.SelectionModel;
+
+/**
+ * Small algorithm that returns a set of vertices that are selected and that have no incoming edges
+ *
+ * @param <V>
+ *            the type of vertices in the graph.
+ * @param <E>
+ *            the type of edges in the graph.
+ * @author Stefan Hahmann
+ */
+public class SelectedRootsFinder< V extends Vertex< E >, E extends Edge< V > > extends AbstractGraphAlgorithm< V, E >
+{
+	private final RefSet< V > roots;
+
+	private final SelectionModel< V, E > selectionModel;
+
+	public SelectedRootsFinder( final ReadOnlyGraph< V, E > graph, final SelectionModel< V, E > selectionModel )
+	{
+		super( graph );
+		this.selectionModel = selectionModel;
+		this.roots = createVertexSet();
+		fetchRoots();
+	}
+
+	public RefSet< V > get()
+	{
+		return roots;
+	}
+
+	private void fetchRoots()
+	{
+		for ( final V vertex : selectionModel.getSelectedVertices() )
+		{
+			if ( vertex.incomingEdges().isEmpty() )
+				roots.add( vertex );
+			if ( !hasSelectedPredecessors( vertex, selectionModel ) )
+				roots.add( vertex );
+		}
+	}
+
+	@SuppressWarnings( { "rawtypes", "unchecked" } )
+	public static < V extends Vertex< ? > > RefSet< V > getRoots( final ReadOnlyGraph< V, ? > graph,
+			final SelectionModel< V, ? > selectionModel )
+	{
+		return new SelectedRootsFinder( graph, selectionModel ).get();
+	}
+
+	private boolean hasSelectedPredecessors( final V vertex, final SelectionModel< V, E > selectionModel )
+	{
+		for ( final E edge : vertex.incomingEdges() )
+		{
+			if ( selectionModel.isSelected( edge.getSource() ) )
+				return true;
+		}
+		return false;
+	}
+}
