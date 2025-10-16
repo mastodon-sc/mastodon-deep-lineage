fd45b346056f866114b365a869fa915c88312987
Stefan Hahmann <stefan.hahmann@tu-dresden.de>
2025-10-16 14:42:36 +0200

    Add 2D linking test for TrackastraLinker with new project resources

--- src/test/java/org/mastodon/mamut/linking/trackastra/TrackastraLinkerTest.java
+++ src/test/java/org/mastodon/mamut/linking/trackastra/TrackastraLinkerTest.java
@@ -32,14 +32,16 @@ import mpicbg.spim.data.SpimDataException;
 
 class TrackastraLinkerTest
 {
-	private final Path tinyExampleProject = resourceAsFile( "/org/mastodon/mamut/linking/trackastra/tgmm-mini_27_31.mastodon" );
+	private final Path exampleProject3D = resourceAsFile( "/org/mastodon/mamut/linking/trackastra/tgmm-mini_27_31.mastodon" );
+
+	private final Path exampleProject2D = resourceAsFile( "/org/mastodon/mamut/linking/trackastra/ctc_fluo_n2dl_hela_11_15.mastodon" );
 
 	private final Context context = new Context();
 
 	@Test
-	void testLinking() throws IOException, SpimDataException, NoSuchFieldException, IllegalAccessException
+	void testLinking3D() throws IOException, SpimDataException, NoSuchFieldException, IllegalAccessException
 	{
-		final MamutProject project = MamutProjectIO.load( tinyExampleProject.toFile().getAbsolutePath() );
+		final MamutProject project = MamutProjectIO.load( exampleProject3D.toFile().getAbsolutePath() );
 		final ProjectModel appModel = ProjectLoader.open( project, context, false, true );
 		ModelGraph graph = appModel.getModel().getGraph();
 		assertEquals( 10, graph.vertices().size() );
@@ -67,6 +69,41 @@ class TrackastraLinkerTest
 		linker.mutate1( graph, appModel.getModel().getSpatioTemporalIndex() );
 		assertEquals( 10, graph.vertices().size() );
 		assertEquals( 8, graph.edges().size() );
+		// close the project
+		appModel.close();
+	}
+
+	@Test
+	void testLinking2D() throws IOException, SpimDataException, NoSuchFieldException, IllegalAccessException
+	{
+		final MamutProject project = MamutProjectIO.load( exampleProject2D.toFile().getAbsolutePath() );
+		final ProjectModel appModel = ProjectLoader.open( project, context, false, true );
+		ModelGraph graph = appModel.getModel().getGraph();
+		assertEquals( 13, graph.vertices().size() );
+		assertEquals( 0, graph.edges().size() );
+		TrackastraLinker< Spot, Link > linker = new TrackastraLinker<>();
+		Map< String, Object > settingsMap = TrackastraUtils.getDefaultTrackAstraSettingsMap();
+		settingsMap.put( KEY_MAX_TIMEPOINT, 4 );
+		settingsMap.put( KEY_SOURCE, appModel.getSharedBdvData().getSources().get( 0 ).getSpimSource() );
+		settingsMap.put( KEY_NUM_DIMENSIONS, 2 );
+		settingsMap.put( KEY_EDGE_THRESHOLD, 0 );
+		Field settings = AbstractGraphParticleLinkerOp.class.getDeclaredField( "settings" );
+		settings.setAccessible( true );
+		settings.set( linker, settingsMap );
+
+		org.scijava.log.Logger log = context.getService( org.scijava.log.LogService.class ).subLogger( "TrackastraLinkerTest" );
+		Field logger = AbstractGraphParticleLinkerOp.class.getDeclaredField( "logger" );
+		logger.setAccessible( true );
+		logger.set( linker, log );
+
+		Field edgeCreator = AbstractGraphParticleLinkerOp.class.getDeclaredField( "edgeCreator" );
+		EdgeCreator< Spot > spotEdgeCreator = new MyEdgeCreator( appModel.getModel().getGraph() );
+		edgeCreator.setAccessible( true );
+		edgeCreator.set( linker, spotEdgeCreator );
+
+		linker.mutate1( graph, appModel.getModel().getSpatioTemporalIndex() );
+		assertEquals( 13, graph.vertices().size() );
+		assertEquals( 11, graph.edges().size() );
 		// close the project
 		appModel.close();
 	}
