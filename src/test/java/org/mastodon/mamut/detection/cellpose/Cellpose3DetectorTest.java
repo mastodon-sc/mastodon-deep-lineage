16009f338920c7a1c754bb1583a31673f11fa78a
hahmann <hahmann@mpi-cbg.de>
2026-04-29 10:48:26 +0200

    Disable Cellpose3Detector test

--- src/test/java/org/mastodon/mamut/detection/cellpose/Cellpose3DetectorTest.java
+++ src/test/java/org/mastodon/mamut/detection/cellpose/Cellpose3DetectorTest.java
@@ -62,7 +62,7 @@ import io.scif.img.ImgOpener;
 class Cellpose3DetectorTest
 {
 
-	//@Disabled( "This test is disabled, because it has very long runtime (> 2 minutes)" )
+	@Disabled( "This test is disabled, because it has very long runtime (> 2 minutes)" )
 	@Test
 	void testCompute3D() throws IllegalAccessException, URISyntaxException, NoSuchFieldException
 	{
